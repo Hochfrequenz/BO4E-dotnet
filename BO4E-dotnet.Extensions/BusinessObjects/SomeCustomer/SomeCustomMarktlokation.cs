84e613e6190273350e1a0eb5f1b4995c1c27cf7a
Konstantin Klein <konstantin.klein@hochfrequenz.de>
2020-03-03 17:03:33 +0100

    idea

---
+++ BO4E-dotnet.Extensions/BusinessObjects/SomeCustomer/SomeCustomMarktlokation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using BO4E.COM;
+using BO4E.BO;
+using System.Linq;
+
+namespace BO4E_dotnet.Extensions.BusinessObjects.SomeCustomer
+{
+    public class MarktlokationTestExtension : BO4E.BO.Marktlokation
+    {
+        public List<Messlokationszuordnung> testList;
+
+        /// <summary>
+        /// create a custom Marklokation busines object where associated melos are part of the malo object itself.
+        /// </summary>
+        /// <param name="malo">a Marktlokation with non-null associated Messlokationen</param>
+        /// <param name="melos">unordered set of messlokations</param>
+        public MarktlokationTestExtension(Marktlokation malo, IEnumerable<Messlokation> melos)
+        {
+            if (melos.Any(melo => !melo.IsValid()))
+            {
+                throw new ArgumentException("There are invalid Messlokations", nameof(melos));
+            }
+            if (malo.zugehoerigeMesslokationen == null)
+            {
+                throw new ArgumentException($"The list of associated melos must not be null {nameof(Marktlokation)}{nameof(Marktlokation.zugehoerigeMesslokationen)}", nameof(malo));
+            }
+            throw new NotImplementedException("ToDo: Fill the list based on malo->messlokationszuordnungen + list of melos");
+        }
+        public (Marktlokation, IEnumerable<Messlokation>) Resolve()
+        {
+            throw new NotImplementedException("has to be programmed");
+        }
+    }
+}
