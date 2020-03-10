741909f3dabd70de07858b823d4a19700d142b26
Hamid <hamid.hajiparvaneh@hochfrequenz.de>
2020-03-10 10:33:20 +0100

    unittest zurück setzen

--- TestBO4E-dotnet-Reporting/TestReportToCsv.cs
+++ TestBO4E-dotnet-Reporting/TestReportToCsv.cs
@@ -54,7 +54,7 @@ namespace TestBO4E.Reporting
             string decimalSeparator = Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator;
             Assert.AreEqual("DE12345;0" + decimalSeparator + "87;2019-01-01T00:00:00Z;2019-03-01T00:00:00Z;", lines[1]);
             var commaResult = cr.ToCsv(',', lineTerminator: Environment.NewLine, reihenfolge: reihenfolge);
-            Assert.AreEqual("DE12345,\"0" + decimalSeparator + "87\",2019-01-01T00:00:00Z,2019-03-01T00:00:00Z,", commaResult.Split(Environment.NewLine)[1]);
+            Assert.AreEqual("DE12345,0" + decimalSeparator + "87,2019-01-01T00:00:00Z,2019-03-01T00:00:00Z,", commaResult.Split(Environment.NewLine)[1]);
             var dpunktResult = cr.ToCsv(':', lineTerminator: Environment.NewLine, reihenfolge: reihenfolge);
             Assert.AreEqual("DE12345:0" + decimalSeparator + "87:\"2019-01-01T00:00:00Z\":\"2019-03-01T00:00:00Z\":", dpunktResult.Split(Environment.NewLine)[1]);
 
