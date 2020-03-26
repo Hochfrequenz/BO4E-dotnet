8cb6d746ee7d8ab4080220914afb65b83c2c30a4
Konstantin Klein <konstantin.klein@hochfrequenz.de>
2020-03-26 22:03:44 +0100

    move stuff out of obsolete class

--- BO4E-dotnet/BoMapper.cs
+++ BO4E-dotnet/BoMapper.cs
@@ -409,49 +409,5 @@ namespace BO4E
                 .OrderBy(af => af.GetCustomAttribute<JsonPropertyAttribute>()?.Order)
                 .ToArray<FieldInfo>();
         }
-
-        /// <summary>
-        /// The UserPropertiesContractResolver allows to put non-BO4E-standard/custom fields/properties into a "userProperties" object.
-        /// </summary>
-        public class UserPropertiesDataContractResolver : DefaultContractResolver
-        {
-            public static readonly UserPropertiesDataContractResolver Instance = new UserPropertiesDataContractResolver(new HashSet<string>());
-
-            private readonly HashSet<string> whitelist;
-
-            /// <summary>
-            /// The UserPropertiesDataContractResolver is initialised with a white list of allowed properties. Everything else is discarded.
-            /// </summary>
-            /// <param name="userPropertiesWhiteList">white list of properties (actually a white"set")</param>
-            public UserPropertiesDataContractResolver(HashSet<string> userPropertiesWhiteList)
-            {
-                whitelist = userPropertiesWhiteList;
-            }
-
-            public override JsonContract ResolveContract(Type type)
-            {
-                JsonContract contract = base.ResolveContract(type);
-                if (contract is JsonObjectContract objContract)
-                {
-                    if (objContract.ExtensionDataSetter != null)
-                    {
-                        ExtensionDataSetter oldSetter = objContract.ExtensionDataSetter;
-                        objContract.ExtensionDataSetter = (o, key, value) =>
-                        {
-                            if (whitelist.Contains(key))
-                            {
-                                oldSetter(o, key, value);
-                            }
-                            else
-                            {
-                                int a = 0;
-                                a++;
-                            }
-                        };
-                    }
-                }
-                return contract;
-            }
-        }
     }
 }
