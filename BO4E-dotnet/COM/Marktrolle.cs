199b02bc8a057bc21181dbea116614bba065826a
Konstantin Klein <konstantin.klein@hochfrequenz.de>
2020-05-06 17:27:28 +0200

    mark COM.Marktrolle as obsolete

--- BO4E-dotnet/COM/Marktrolle.cs
+++ BO4E-dotnet/COM/Marktrolle.cs
@@ -1,8 +1,10 @@
-﻿using BO4E.meta;
+﻿using BO4E.BO;
+using BO4E.meta;
 
 using Newtonsoft.Json;
 
 using ProtoBuf;
+using System;
 
 namespace BO4E.COM
 {
@@ -11,6 +13,7 @@ namespace BO4E.COM
     /// </summary>
     [ProtoContract]
     [NonOfficial(NonOfficialCategory.UNSPECIFIED)]
+    [Obsolete("Please use BO4E.BO."+nameof(Marktteilnehmer)+" instead.")]
     public class Marktrolle : COM
     {
         /// <summary>
@@ -18,6 +21,7 @@ namespace BO4E.COM
         /// </summary>
         [JsonProperty(PropertyName = "rollencodenummer", Required = Required.Default)]
         [ProtoMember(3)]
+        [Obsolete("Use "+nameof(Marktteilnehmer)+"."+nameof(Marktteilnehmer.Rollencodenummer) +" instead")]
         public string Rollencodenummer { get; set; }
 
         /// <summary>
@@ -25,6 +29,7 @@ namespace BO4E.COM
         /// </summary>
         [JsonProperty(PropertyName = "code", Required = Required.Default)]
         [NonOfficial(NonOfficialCategory.UNSPECIFIED)]
+        [Obsolete("Use " + nameof(Marktteilnehmer) + "." + nameof(Marktteilnehmer.Rollencodetyp) + " instead")]
         [ProtoMember(4)]
         public string Code { get; set; }
 
@@ -33,6 +38,7 @@ namespace BO4E.COM
         /// </summary>
         [JsonProperty(Required = Required.Always)]
         [ProtoMember(5)]
+        [Obsolete("Use " + nameof(Marktteilnehmer) + "." + nameof(Marktteilnehmer.Marktrolle) + " instead")]
 #pragma warning disable IDE1006 // Naming Styles because Marktrolle is already the name of the enum
         public ENUM.Marktrolle marktrolle { get; set; }
 #pragma warning restore IDE1006 // Naming Styles
