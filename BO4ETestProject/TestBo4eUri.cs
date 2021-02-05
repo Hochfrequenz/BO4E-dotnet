27f9570d8e4fe1197e0011e6584b9be5e6e57c2c
konstantin <konstantin.klein@hochfrequenz.de>
2021-02-05 12:05:13 +0100

    autoformat to break overlong lines

--- BO4ETestProject/TestBo4eUri.cs
+++ BO4ETestProject/TestBo4eUri.cs
@@ -1,13 +1,9 @@
 ﻿using BO4E.BO;
 using BO4E.meta;
-
 using JsonDiffPatchDotNet;
-
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -30,9 +26,11 @@ namespace TestBO4E
                     var jsonString = r.ReadToEnd();
                     json = JsonConvert.DeserializeObject<JObject>(jsonString);
                 }
+
                 Assert.IsNotNull(json, $"The content of file {file} seems to be no valid JSON.");
                 Assert.IsNotNull(json["input"], $"The file {file} does not contain the mandatory 'input' key.");
-                Assert.IsNotNull(json["expectedUri"], $"The file {file} does not contain the mandatory 'expectedUri' key.");
+                Assert.IsNotNull(json["expectedUri"],
+                    $"The file {file} does not contain the mandatory 'expectedUri' key.");
                 //string boType = (string)json["input"]["boTyp"];
                 //Assert.IsNotNull(boType, $"The JSON content of file {file} is missing the obligatory 'boTyp' attribute.");
                 var bo = JsonConvert.DeserializeObject<BusinessObject>(json["input"].ToString());
@@ -42,7 +40,8 @@ namespace TestBO4E
                 var uriString = uri.AbsoluteUri;
                 Assert.AreEqual(json["expectedUri"], uriString, "The URI doesn't match the expectations.");
 
-                Assert.IsNotNull(json["expectedQueryObject"], $"Please specify the query object result in file {file}, key 'expectedQueryObject'.");
+                Assert.IsNotNull(json["expectedQueryObject"],
+                    $"Please specify the query object result in file {file}, key 'expectedQueryObject'.");
                 var queryObject = uri.GetQueryObject();
                 var jdp = new JsonDiffPatch();
                 var left = json["expectedQueryObject"];
@@ -57,6 +56,7 @@ namespace TestBO4E
                     {
                         continue;
                     }
+
                     Assert.IsNull(patch, patch.ToString());
                 }
                 else
@@ -81,6 +81,7 @@ namespace TestBO4E
                 // must not happen!
                 exceptionThrown = true;
             }
+
             Assert.IsFalse(exceptionThrown);
             Assert.IsTrue(result.Contains("ENERGIEMENGE"));
         }
@@ -88,13 +89,14 @@ namespace TestBO4E
         [TestMethod]
         public void TestWellformedKannmichmal()
         {
-            var uri = new Bo4eUri("bo4e://messlokation/?filter=bilanzierungsmethode eq 'RLM'or bilanzierungsmethode eq 'IMS'");
+            var uri = new Bo4eUri(
+                "bo4e://messlokation/?filter=bilanzierungsmethode eq 'RLM'or bilanzierungsmethode eq 'IMS'");
             Assert.IsNotNull(uri);
         }
 
         private static readonly Dictionary<string, string> boNameResults = new Dictionary<string, string>
         {
-            {"bo4e://Marktlokation/987654321098", "Marktlokation" },
+            {"bo4e://Marktlokation/987654321098", "Marktlokation"},
             {"bo4e://hurzelasdanoafi/123456", null},
             {"bo4e://Energiemenge/123456", "Energiemenge"},
             {"bo4e://Ansprechpartner/123467/adsadad/aafe4rq3rqr/", "Ansprechpartner"},
@@ -111,7 +113,8 @@ namespace TestBO4E
                 try
                 {
                     var uri = new Bo4eUri(testString);
-                    Assert.AreEqual(boNameResults[testString], uri.GetBoName(), $"boName validation failed for {testString}.");
+                    Assert.AreEqual(boNameResults[testString], uri.GetBoName(),
+                        $"boName validation failed for {testString}.");
                     Assert.IsTrue(uri.GetBoType().ToString().EndsWith(boNameResults[testString]));
                 }
                 catch (ArgumentException)
@@ -123,8 +126,10 @@ namespace TestBO4E
 
         private static readonly Dictionary<Type, List<string>> boKeyNamesResults = new Dictionary<Type, List<string>>
         {
-            {typeof(Marktlokation), new List<string>{"marktlokationsId"}},
-            {typeof(Messlokation), new List<string>{"messlokationsId"} } //<-- should be the json property name if annotated 
+            {typeof(Marktlokation), new List<string> {"marktlokationsId"}},
+            {
+                typeof(Messlokation), new List<string> {"messlokationsId"}
+            } //<-- should be the json property name if annotated 
         };
 
         [TestMethod]
@@ -134,21 +139,22 @@ namespace TestBO4E
             {
                 var expectedList = boKeyNamesResults[boType];
                 var actualList = BusinessObject.GetBoKeyNames(boType);
-                Assert.IsTrue(expectedList.SequenceEqual(actualList), $"{boType}: expected: [{string.Join(",", expectedList)}] actual: [{string.Join(",", actualList)}] ");
+                Assert.IsTrue(expectedList.SequenceEqual(actualList),
+                    $"{boType}: expected: [{string.Join(",", expectedList)}] actual: [{string.Join(",", actualList)}] ");
             }
         }
 
         private static readonly Dictionary<string, bool> validationResults = new Dictionary<string, bool>
         {
-            {"bo4e://Marktlokation/987654321098", true },
-           // {"   bo4e://Leerzeichen/987654321098", false },
+            {"bo4e://Marktlokation/987654321098", true},
+            // {"   bo4e://Leerzeichen/987654321098", false },
             {"keinProtokoll/123456", false},
             {"falschesProtokoll://asdadadl/123456", false},
-            {"bo4e://Marktlokation/123467/adsadad/aafe4rq3rqr/",true},
-            {"bo4e://kf56@Marktlokation:100/adadsadad", true },
-            {"bo4e://kf56:pw@Marktlokation:100/adadsadad?dasd=asd", true },
-            {"bo4e://Marktlokation/123467/adsadad/aafe4rq3rqr?asdasda=3r343&adasdas=2334#341", true },
-            {"bo4e://Marktteilnehmer/?backendId=1234", true }
+            {"bo4e://Marktlokation/123467/adsadad/aafe4rq3rqr/", true},
+            {"bo4e://kf56@Marktlokation:100/adadsadad", true},
+            {"bo4e://kf56:pw@Marktlokation:100/adadsadad?dasd=asd", true},
+            {"bo4e://Marktlokation/123467/adsadad/aafe4rq3rqr?asdasda=3r343&adasdas=2334#341", true},
+            {"bo4e://Marktteilnehmer/?backendId=1234", true}
         };
 
         [TestMethod]
@@ -156,14 +162,16 @@ namespace TestBO4E
         {
             foreach (var testString in validationResults.Keys)
             {
-                Assert.AreEqual(validationResults[testString], Bo4eUri.IsValid(testString), $"URI validation failed for {testString} .");
+                Assert.AreEqual(validationResults[testString], Bo4eUri.IsValid(testString),
+                    $"URI validation failed for {testString} .");
             }
         }
 
         [TestMethod]
         public void TestUPInclusion()
         {
-            var emString = @"{'versionStruktur':1,'boTyp':'ENERGIEMENGE','lokationsId':'DE0000000000000000000000010000400','lokationstyp':'MeLo','zw':'000000000030000301','anlagennummer':'4000000199','messlokationsId':'DE0000000000000000000000010000400','marktlokationsId':''}";
+            var emString =
+                @"{'versionStruktur':1,'boTyp':'ENERGIEMENGE','lokationsId':'DE0000000000000000000000010000400','lokationstyp':'MeLo','zw':'000000000030000301','anlagennummer':'4000000199','messlokationsId':'DE0000000000000000000000010000400','marktlokationsId':''}";
             var em = JsonConvert.DeserializeObject<Energiemenge>(emString);
             Assert.IsNotNull(em.UserProperties);
             Assert.IsTrue(em.UserProperties.Keys.Count > 0);
@@ -175,10 +183,14 @@ namespace TestBO4E
         [TestMethod]
         public void TestRoundTripUriFilterQueryObject()
         {
-            var qo = JObject.Parse("{'marktlokationsId':'543212345', 'messlokationsId':'DE123', 'bilanzierungsmethode':'SLP'}");
-            var uri = new Bo4eUri("bo4e://marktlokation?search=something").AddFilter(JsonConvert.DeserializeObject<IDictionary<string, object>>(qo.ToString()));
+            var qo = JObject.Parse(
+                "{'marktlokationsId':'543212345', 'messlokationsId':'DE123', 'bilanzierungsmethode':'SLP'}");
+            var uri = new Bo4eUri("bo4e://marktlokation?search=something").AddFilter(
+                JsonConvert.DeserializeObject<IDictionary<string, object>>(qo.ToString()));
             Assert.IsNotNull(uri);
-            Assert.AreEqual("bo4e://marktlokation/?search=something&filter=marktlokationsId+eq+%27543212345%27+and+bilanzierungsmethode+eq+%27SLP%27", uri.ToString());
+            Assert.AreEqual(
+                "bo4e://marktlokation/?search=something&filter=marktlokationsId+eq+%27543212345%27+and+bilanzierungsmethode+eq+%27SLP%27",
+                uri.ToString());
             var qo2 = uri.GetQueryObject();
             Assert.IsTrue(qo2.ContainsKey("marktlokationsId"));
             Assert.IsTrue(qo2.ContainsKey("bilanzierungsmethode"));
