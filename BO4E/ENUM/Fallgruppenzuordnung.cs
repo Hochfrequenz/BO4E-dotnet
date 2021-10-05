c1d71534f0b787312da6b631543e8eb1f44eb1d4
Joscha Metze <joscha.metze@hochfrequenz.de>
2021-10-05 12:59:39 +0200

    remove link as sepcial chars are not good in xml documentation

--- BO4E/ENUM/Fallgruppenzuordnung.cs
+++ BO4E/ENUM/Fallgruppenzuordnung.cs
@@ -2,7 +2,7 @@ using BO4E.meta;
 
 namespace BO4E.ENUM
 {
-    /// <summary>Fallgruppenzuordnung nach edi@energy (https://www.edi-energy.de/index.php?id=38&tx_bdew_bdew%5Buid%5D=1203&tx_bdew_bdew%5Baction%5D=download&tx_bdew_bdew%5Bcontroller%5D=Dokument&cHash=dfbb27270e6f18ea28b534007d7a9783)</summary>
+    /// <summary>Fallgruppenzuordnung nach edi@energy </summary>
     [NonOfficial(NonOfficialCategory.REGULATORY_REQUIREMENTS)]
     public enum Fallgruppenzuordnung
     {
