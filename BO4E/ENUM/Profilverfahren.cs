ff56026f187b737e6dacbd8c66440bb9f40de53e
Kevin <kevin.krechan@hochfrequenz.de>
2022-01-11 16:38:58 +0100

    ✨ Add BO Bilanzierung and its dependencies

---
+++ BO4E/ENUM/Profilverfahren.cs
@@ -0,0 +1,15 @@
+﻿using BO4E.meta;
+
+namespace BO4E.ENUM
+{
+    /// <summary>Profilverfahren (synthetisch/ analytisch)</summary>
+    [NonOfficial(NonOfficialCategory.REGULATORY_REQUIREMENTS)]
+    public enum Profilverfahren
+    {
+        /// <summary>SLP</summary>
+        SYNTHETISCH,
+
+        /// <summary>ALP</summary>
+        ANALYTISCH
+    }
+}
