8a1650529a56b381c7afae138513c49f2b9859b1
Torsten Ueberschar <tu@uesome.de>
2022-02-01 14:11:38 +0100

    add enum for WahlrechtPrognosegrundlage

---
+++ BO4E/ENUM/WahlrechtPrognosegrundlage.cs
@@ -0,0 +1,33 @@
+namespace BO4E.ENUM
+{
+    /// <summary>
+    /// Wahlrecht der Prognosegrundlage der Marktlokation
+    /// </summary>
+    public enum WahlrechtPrognosegrundlage
+    {
+        /// <summary>
+        /// Wahlrecht durch LF gegeben
+        /// </summary>
+        LF,
+
+        /// <summary>
+        /// Wahlrecht nicht vorhanden wegen Verbrauch > 10.000 kWh/a
+        /// </summary>
+        NICHT_WEGEN_GROSSER_VERBRAUCH,
+
+        /// <summary>
+        /// Wahlrecht nicht vorhanden wegen Eigenverbrauch
+        /// </summary>
+        NICHT_WEGEN_EIGENVERBRAUCH,
+
+        /// <summary>
+        /// Wahlrecht nicht vorhanden wegen tagesparameterabhängigem Verbrauch
+        /// </summary>
+        NICHT_WEGEN_TAGES_VERBRAUCH,
+
+        /// <summary>
+        /// Wahlrecht nicht vorhanden wegen §14a EnWG
+        /// </summary>
+        NICHT_WEGEN_ENWG
+    }
+}
