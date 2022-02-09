085d80aebe95027d9d10d27f9adad6b73fa3efe0
Franziska Vesely <franziska.vesely@hochfrequenz.de>
2022-02-09 16:29:04 +0100

    add Taristufe enum for iftsta

---
+++ BO4E/ENUM/Tarifstufe.cs
@@ -0,0 +1,50 @@
+using BO4E.meta;
+
+namespace BO4E.ENUM
+{
+    /// <summary>
+    /// In IFTSTA 21035 "Rückmeldung auf Lieferschein" (IFTSTA SG16 QTY 6063) 
+    /// </summary>
+    [NonOfficial(NonOfficialCategory.UNSPECIFIED)]
+
+    public enum Tarifstufe
+    {
+        /// <summary> TARIFSTUFE_0 </summary>
+        /// <remarks>Z20</remarks>
+        TARIFSTUFE_0,
+
+        /// <summary> TARIFSTUFE_1 </summary>
+        /// <remarks>Z21</remarks>
+        TARIFSTUFE_1,
+
+        /// <summary> TARIFSTUFE_2 </summary>
+        /// <remarks>Z22</remarks>
+        TARIFSTUFE_2,
+
+        /// <summary> TARIFSTUFE_3 </summary>
+        /// <remarks>Z23</remarks>
+        TARIFSTUFE_3,
+        /// <summary> TARIFSTUFE_4 </summary>
+        /// <remarks>Z24</remarks>
+        TARIFSTUFE_4,
+        /// <summary> TARIFSTUFE_5 </summary>
+        /// <remarks>Z25</remarks>
+        TARIFSTUFE_5,
+
+        /// <summary> TARIFSTUFE_6 </summary>
+        /// <remarks>Z26</remarks>
+        TARIFSTUFE_6,
+
+        /// <summary> TARIFSTUFE_7 </summary>
+        /// <remarks>Z27</remarks>
+        TARIFSTUFE_7,
+
+        /// <summary> TARIFSTUFE_8 </summary>
+        /// <remarks>Z28</remarks>
+        TARIFSTUFE_8,
+
+        /// <summary> TARIFSTUFE_9 </summary>
+        /// <remarks>Z29</remarks>
+        TARIFSTUFE_9
+    }
+}
