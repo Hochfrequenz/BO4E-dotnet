e7991aeb25740cd5110268469c97e91b9215f246
Annika <73470827+hf-aschloegl@users.noreply.github.com>
2022-02-09 14:04:09 +0100

    TST-937/TST-938: Add more values to Status Enum (#120)
    * Add more values to Status Enum
    
    * Generated BO4ENET.xml through build
    
    * dotnet-format
    
    * Double Enum
    
    * Generated files
    
    * Add Spannungsausfall
    
    * Generated files

--- BO4E/ENUM/Status.cs
+++ BO4E/ENUM/Status.cs
@@ -60,6 +60,43 @@ namespace BO4E.ENUM
         /// <remarks>Z39</remarks>
         ZAEHLERSTAND_ZUM_ENDE_DER_ANGEGEBENEN_ENERGIEMENGE_NICHT_VORHANDEN_DA_MENGENABGRENZUNG,
 
+        /// <summary> KEIN_ZUGANG </summary>
+        /// <remarks>Z74</remarks>
+        KEIN_ZUGANG,
+
+        /// <summary> KOMMUNIKATIONSSTOERUNG </summary>
+        /// <remarks>Z75</remarks>
+        KOMMUNIKATIONSSTOERUNG,
+
+        /// <summary> NETZAUSFALL </summary>
+        /// <remarks>Z76</remarks>
+        NETZAUSFALL,
+
+        /// <summary> SPANNUNGSAUSFALL </summary>
+        /// <remarks>Z77</remarks>
+        SPANNUNGSAUSFALL,
+
+        /// <summary> STATUS_GERAETEWECHSEL </summary>
+        /// <remarks>Needs to have a prefix, because this is also an enum value in <see cref="Themengebiet" /></remarks>
+        /// <remarks>Z78</remarks>
+        STATUS_GERAETEWECHSEL,
+
+        /// <summary> KALIBRIERUNG </summary>
+        /// <remarks>Z79</remarks>
+        KALIBRIERUNG,
+
+        /// <summary> GERAET_ARBEITET_AUSSERHALB_DER_BETRIEBSBEDINGUNGEN </summary>
+        /// <remarks>Z80</remarks>
+        GERAET_ARBEITET_AUSSERHALB_DER_BETRIEBSBEDINGUNGEN,
+
+        /// <summary> MESSEINRICHTUNG_GESTOERT_DEFEKT </summary>
+        /// <remarks>Z81</remarks>
+        MESSEINRICHTUNG_GESTOERT_DEFEKT,
+
+        /// <summary> UNSICHERHEIT_MESSUNG </summary>
+        /// <remarks>Z82</remarks>
+        UNSICHERHEIT_MESSUNG,
+
         /// <summary> KUNDENSELBSTABLESUNG </summary>
         /// <remarks>Z83</remarks>
         KUNDENSELBSTABLESUNG,
@@ -80,8 +117,148 @@ namespace BO4E.ENUM
         /// <remarks>Z87</remarks>
         PLAUSIBEL_WG_KUNDENHINWIES,
 
+        /// <summary> VERGLEICHSMESSUNG_GEEICHT </summary>
+        /// <remarks>Z88</remarks>
+        VERGLEICHSMESSUNG_GEEICHT,
+
+        /// <summary> VERGLEICHSMESSUNG_NICHT_GEEICHT </summary>
+        /// <remarks>Z89</remarks>
+        VERGLEICHSMESSUNG_NICHT_GEEICHT,
+
+        /// <summary> MESSWERTNACHBILDUNG_AUS_GEEICHTEN_WERTEN </summary>
+        /// <remarks>Z90</remarks>
+        MESSWERTNACHBILDUNG_AUS_GEEICHTEN_WERTEN,
+
+        /// <summary> MESSWERTNACHBILDUNG_AUS_NICHT_GEEICHTEN_WERTEN </summary>
+        /// <remarks>Z91</remarks>
+        MESSWERTNACHBILDUNG_AUS_NICHT_GEEICHTEN_WERTEN,
+
+        /// <summary> INTERPOLATION </summary>
+        /// <remarks>Z92</remarks>
+        INTERPOLATION,
+
+        /// <summary> HALTEWERT </summary>
+        /// <remarks>Z93</remarks>
+        HALTEWERT,
+
+        /// <summary> BILANZIERUNG_NETZABSCHNITT </summary>
+        /// <remarks>Z94</remarks>
+        BILANZIERUNG_NETZABSCHNITT,
+
+        /// <summary> HISTORISCHE_MESSWERTE </summary>
+        /// <remarks>Z95</remarks>
+        HISTORISCHE_MESSWERTE,
+
+        /// <summary> BERUECKSICHTIGUNG_STOERMENGENZAEHLWERK </summary>
+        /// <remarks>Z98</remarks>
+        BERUECKSICHTIGUNG_STOERMENGENZAEHLWERK,
+
+        /// <summary> MENGENUMWERTUNG_VOLLSTAENDIG </summary>
+        /// <remarks>Z99</remarks>
+        MENGENUMWERTUNG_VOLLSTAENDIG,
+
+        /// <summary> UHRZEIT_GESTELLT_SYNCHRONISATION </summary>
+        /// <remarks>ZA0</remarks>
+        UHRZEIT_GESTELLT_SYNCHRONISATION,
+
+        /// <summary> MESSWERT_UNPLAUSIBEL </summary>
+        /// <remarks>ZA1</remarks>
+        MESSWERT_UNPLAUSIBEL,
+
+        /// <summary> FALSCHER_WANDLERFAKTOR </summary>
+        /// <remarks>ZA3</remarks>
+        FALSCHER_WANDLERFAKTOR,
+
+        /// <summary> FEHLERHAFTE_ABLESUNG </summary>
+        /// <remarks>ZA4</remarks>
+        FEHLERHAFTE_ABLESUNG,
+
+        /// <summary> AENDERUNG_DER_BERECHNUNG </summary>
+        /// <remarks>ZA5</remarks>
+        AENDERUNG_DER_BERECHNUNG,
+
+        /// <summary> UMBAU_DER_MESSLOKATION </summary>
+        /// <remarks>ZA6</remarks>
+        UMBAU_DER_MESSLOKATION,
+
+        /// <summary> DATENBEARBEITUNGSFEHLER </summary>
+        /// <remarks>ZA7</remarks>
+        DATENBEARBEITUNGSFEHLER,
+
+        /// <summary> BRENNWERTKORREKTUR </summary>
+        /// <remarks>ZA8</remarks>
+        BRENNWERTKORREKTUR,
+
+        /// <summary> Z_ZAHL_KORREKTUR </summary>
+        /// <remarks>ZA9</remarks>
+        Z_ZAHL_KORREKTUR,
+
+        /// <summary> STOERUNG_DEFEKT_MESSEINRICHTUNG </summary>
+        /// <remarks>ZB0</remarks>
+        STOERUNG_DEFEKT_MESSEINRICHTUNG,
+
+        /// <summary> AENDERUNG_TARIFSCHALTZEITEN </summary>
+        /// <remarks>ZB9</remarks>
+        AENDERUNG_TARIFSCHALTZEITEN,
+
+        /// <summary> TARIFSCHALTGERAET_DEFEKT </summary>
+        /// <remarks>ZC2</remarks>
+        TARIFSCHALTGERAET_DEFEKT,
+
         /// <summary> AUSTAUSCH_DES_ERSATZWERTES </summary>
         /// <remarks>ZC3</remarks>
         AUSTAUSCH_DES_ERSATZWERTES,
+
+        /// <summary> IMPULSWERTIGKEIT_NICHT_AUSREICHEND </summary>
+        /// <remarks>ZC4</remarks>
+        IMPULSWERTIGKEIT_NICHT_AUSREICHEND,
+
+        /// <summary> UMSTELLUNG_GASQUALITAET </summary>
+        /// <remarks>ZG3</remarks>
+        UMSTELLUNG_GASQUALITAET,
+
+        /// <summary> STATISTISCHE_METHODE </summary>
+        /// <remarks>ZJ2</remarks>
+        STATISTISCHE_METHODE,
+
+        /// <summary> ENERGIEMENGE_IN_UNGEMESSENEM_ZEITINTERVALL </summary>
+        /// <remarks>ZJ8</remarks>
+        ENERGIEMENGE_IN_UNGEMESSENEM_ZEITINTERVALL,
+
+        /// <summary> ENERGIEMENGE_AUS_DEM_UNGEPAIRTEN_ZEITINTERVALL </summary>
+        /// <remarks>ZJ9</remarks>
+        ENERGIEMENGE_AUS_DEM_UNGEPAIRTEN_ZEITINTERVALL,
+
+        /// <summary> AUFTEILUNG </summary>
+        /// <remarks>ZQ8</remarks>
+        AUFTEILUNG,
+
+        /// <summary> VERWENDUNG_VON_WERTEN_DES_STOERMENGENZAEHLWERKS </summary>
+        /// <remarks>ZQ9</remarks>
+        VERWENDUNG_VON_WERTEN_DES_STOERMENGENZAEHLWERKS,
+
+        /// <summary> UMGANGS_UND_KORREKTURMENGEN </summary>
+        /// <remarks>ZR0</remarks>
+        UMGANGS_UND_KORREKTURMENGEN,
+
+        /// <summary> WARTUNGSARBEITEN_AN_GEEICHTEM_MESSGERAET </summary>
+        /// <remarks>ZR1</remarks>
+        WARTUNGSARBEITEN_AN_GEEICHTEM_MESSGERAET,
+
+        /// <summary> GESTOERTE_WERTE </summary>
+        /// <remarks>ZR2</remarks>
+        GESTOERTE_WERTE,
+
+        /// <summary> WARTUNGSARBEITEN_AN_EICHRECHTSKONFORMEN_MESSGERAETEN </summary>
+        /// <remarks>ZR3</remarks>
+        WARTUNGSARBEITEN_AN_EICHRECHTSKONFORMEN_MESSGERAETEN,
+
+        /// <summary> KONSISTENZ_UND_SYNCHRONPRUEFUNG </summary>
+        /// <remarks>ZR4</remarks>
+        KONSISTENZ_UND_SYNCHRONPRUEFUNG,
+
+        /// <summary> RECHENWERT </summary>
+        /// <remarks>ZR5</remarks>
+        RECHENWERT,
     }
 }
