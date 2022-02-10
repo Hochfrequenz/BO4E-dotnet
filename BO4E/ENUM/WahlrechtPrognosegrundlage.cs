1728a1250d7afc37984dd2c430a16bac65657f9b
konstantin <konstantin.klein@hochfrequenz.de>
2022-02-10 16:57:28 +0100

    TST-884 - add enum WahlrechtPrognosegrundlage (#116)
    * add enum for WahlrechtPrognosegrundlage
    
    * fix distiction of enaum values
    
    * typo
    
    * add remarks
    
    Co-authored-by: konstantin <konstantin.klein@hochfrequenz.de>

---
+++ BO4E/ENUM/WahlrechtPrognosegrundlage.cs
@@ -0,0 +1,39 @@
+namespace BO4E.ENUM
+{
+    /// <summary>
+    /// Wahlrecht der Prognosegrundlage der Marktlokation
+    /// </summary>
+    /// <remarks> SG10 CAV </remarks>
+    public enum WahlrechtPrognosegrundlage
+    {
+        /// <summary>
+        /// Wahlrecht durch LF gegeben
+        /// </summary>
+        /// <remarks> CAV+Z54 </remarks>
+        DURCH_LF,
+
+        /// <summary>
+        /// Wahlrecht nicht vorhanden wegen Verbrauch > 10.000 kWh/a
+        /// </summary>
+        /// <remarks> CAV+Z55 </remarks>
+        NICHT_WEGEN_GROSSEN_VERBRAUCHS,
+
+        /// <summary>
+        /// Wahlrecht nicht vorhanden wegen Eigenverbrauch
+        /// </summary>
+        /// <remarks> CAV+ZC1 </remarks>
+        NICHT_WEGEN_EIGENVERBRAUCH,
+
+        /// <summary>
+        /// Wahlrecht nicht vorhanden wegen tagesparameterabhängigem Verbrauch
+        /// </summary>
+        /// <remarks> CAV+ZD2 </remarks>
+        NICHT_WEGEN_TAGES_VERBRAUCH,
+
+        /// <summary>
+        /// Wahlrecht nicht vorhanden wegen §14a EnWG
+        /// </summary>
+        /// <remarks> CAV+ZE3 </remarks>
+        NICHT_WEGEN_ENWG
+    }
+}
