1f94a21483deaf046b16447d54b32e23223acd8b
Franziska Vesely <franziska.vesely@hochfrequenz.de>
2022-02-15 18:10:27 +0100

    Add Handelsunstimmigkeit BO

---
+++ BO4E/COM/Handelsunstimmigkeitsbegruendung.cs
@@ -0,0 +1,38 @@
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+using BO4E.ENUM;
+using BO4E.meta;
+using Newtonsoft.Json;
+using ProtoBuf;
+
+namespace BO4E.COM
+{
+  /// <summary>
+  /// Begründung der Korrektheit der Rechnung oder des Lieferscheins (COMDIS SG3 AJT)
+  /// </summary>
+  [ProtoContract]
+  [NonOfficial(NonOfficialCategory.MISSING)]
+  public class Handelsunstimmigkeitsbegruendung : COM
+  {
+    /// <summary>
+    /// Referenzen auf vorherige Nachricht
+    /// </summary>
+    [JsonProperty(PropertyName = "referenzen", Required = Required.Default, Order = 1)]
+    [JsonPropertyName("referenzen")]
+    [NonOfficial(NonOfficialCategory.MISSING)]
+    [ProtoMember(1)]
+
+    public List<string> Referenzen { get; set; }
+
+
+    /// <summary>
+    /// Angabe des Handelsunstimmigkeitsgrunds
+    /// </summary>
+    [JsonProperty(PropertyName = "grund", Required = Required.Always, Order = 2)]
+    [JsonPropertyName("grund")]
+    [NonOfficial(NonOfficialCategory.MISSING)]
+    [ProtoMember(2)]
+    public Handelsunstimmigkeitsgrund Grund { get; set; }
+
+  }
+}
---
+++ BO4E/ENUM/Handelsunstimmigkeitstyp.cs
@@ -0,0 +1,28 @@
+using BO4E.meta;
+
+namespace BO4E.ENUM
+{
+  /// <summary>
+  /// Gibt den Typ der Handelsunstimmigkeit an (COMDIS DOC 1001)
+  /// </summary>
+  [NonOfficial(NonOfficialCategory.MISSING)]
+  public enum Handelsunstimmigkeitstyp
+  {
+    /// <summary> HANDELSRECHNUNG </summary>
+    /// <remarks>380</remarks>
+    HANDELSRECHNUNG,
+
+    /// <summary> LIEFERSCHEIN_HANDELSUNSTIMMIGKEITSTYP </summary>
+    /// <remarks>270</remarks>
+    LIEFERSCHEIN_HANDELSUNSTIMMIGKEITSTYP,
+
+    /// <summary> LIEFERSCHEIN_GRUND_ARBEITSPREIS</summary>
+    /// <remarks>Z41</remarks>
+    LIEFERSCHEIN_GRUND_ARBEITSPREIS,
+
+    /// <summary> LIEFERSCHEIN_ARBEITS_LEISTUNGSPREIS </summary>
+    /// <remarks>Z42</remarks>
+    LIEFERSCHEIN_ARBEITS_LEISTUNGSPREIS,
+
+  }
+}
