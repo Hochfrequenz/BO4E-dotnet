7e9a928b207789eda76899cd6c3f21b8fcca4f11
Annika Schlögl <annika.schloegl@hochfrequenz.de>
2022-02-15 18:19:52 +0100

    dotnet-format

--- BO4E/BO/Handelsunstimmigkeit.cs
+++ BO4E/BO/Handelsunstimmigkeit.cs
@@ -13,51 +13,51 @@ using System.Text.Json.Serialization;
 
 namespace BO4E.BO
 {
-  /// <summary>
-  /// Handelsunstimmigkeit BO
-  /// </summary>
-  [ProtoContract]
-  [NonOfficial(NonOfficialCategory.MISSING)]
-  public class Handelsunstimmigkeit : BusinessObject
-  {
-
     /// <summary>
-    /// Handelsunstimmigkeitsnummer
+    /// Handelsunstimmigkeit BO
     /// </summary>
-    [JsonProperty(PropertyName = "nummer", Required = Required.Always)]
-    [JsonPropertyName("nummer")]
+    [ProtoContract]
     [NonOfficial(NonOfficialCategory.MISSING)]
-    [ProtoMember(1000)]
-    [BoKey]
-    public string Nummer { get; set; }
+    public class Handelsunstimmigkeit : BusinessObject
+    {
 
-    /// <summary>
-    /// Gibt den Typ der Handelsunstimmigkeit an.
-    /// </summary>
-    /// <see cref="Handelsunstimmigkeitstyp" />
-    [JsonProperty(PropertyName = "typ", Required = Required.Always)]
-    [JsonPropertyName("typ")]
-    [ProtoMember(1001)]
-    public Handelsunstimmigkeitstyp Typ { get; set; }
+        /// <summary>
+        /// Handelsunstimmigkeitsnummer
+        /// </summary>
+        [JsonProperty(PropertyName = "nummer", Required = Required.Always)]
+        [JsonPropertyName("nummer")]
+        [NonOfficial(NonOfficialCategory.MISSING)]
+        [ProtoMember(1000)]
+        [BoKey]
+        public string Nummer { get; set; }
 
-    /// <summary>
-    /// Handelsunstimmigekeitsbegründung
-    /// </summary>
-    /// <see cref="Handelsunstimmigkeitsbegruendung" />
-    [JsonProperty(PropertyName = "begruendung", Required = Required.Always)]
-    [JsonPropertyName("begruendung")]
-    [NonOfficial(NonOfficialCategory.MISSING)]
-    [ProtoMember(1002)]
-    public Handelsunstimmigkeitsbegruendung Begruendung { get; set; }
+        /// <summary>
+        /// Gibt den Typ der Handelsunstimmigkeit an.
+        /// </summary>
+        /// <see cref="Handelsunstimmigkeitstyp" />
+        [JsonProperty(PropertyName = "typ", Required = Required.Always)]
+        [JsonPropertyName("typ")]
+        [ProtoMember(1001)]
+        public Handelsunstimmigkeitstyp Typ { get; set; }
 
-    /// <summary>
-    /// angeforderter Betrag
-    /// </summary>
-    [JsonProperty(PropertyName = "zuZahlen", Required = Required.Default)]
-    [JsonPropertyName("zuZahlen")]
-    [NonOfficial(NonOfficialCategory.MISSING)]
-    [ProtoMember(1003)]
-    public Betrag ZuZahlen { get; set; }
+        /// <summary>
+        /// Handelsunstimmigekeitsbegründung
+        /// </summary>
+        /// <see cref="Handelsunstimmigkeitsbegruendung" />
+        [JsonProperty(PropertyName = "begruendung", Required = Required.Always)]
+        [JsonPropertyName("begruendung")]
+        [NonOfficial(NonOfficialCategory.MISSING)]
+        [ProtoMember(1002)]
+        public Handelsunstimmigkeitsbegruendung Begruendung { get; set; }
 
-  }
+        /// <summary>
+        /// angeforderter Betrag
+        /// </summary>
+        [JsonProperty(PropertyName = "zuZahlen", Required = Required.Default)]
+        [JsonPropertyName("zuZahlen")]
+        [NonOfficial(NonOfficialCategory.MISSING)]
+        [ProtoMember(1003)]
+        public Betrag ZuZahlen { get; set; }
+
+    }
 }
--- BO4E/COM/Handelsunstimmigkeitsbegruendung.cs
+++ BO4E/COM/Handelsunstimmigkeitsbegruendung.cs
@@ -7,32 +7,32 @@ using ProtoBuf;
 
 namespace BO4E.COM
 {
-  /// <summary>
-  /// Begründung der Korrektheit der Rechnung oder des Lieferscheins (COMDIS SG3 AJT)
-  /// </summary>
-  [ProtoContract]
-  [NonOfficial(NonOfficialCategory.MISSING)]
-  public class Handelsunstimmigkeitsbegruendung : COM
-  {
     /// <summary>
-    /// Referenzen auf vorherige Nachricht
+    /// Begründung der Korrektheit der Rechnung oder des Lieferscheins (COMDIS SG3 AJT)
     /// </summary>
-    [JsonProperty(PropertyName = "referenzen", Required = Required.Default, Order = 1)]
-    [JsonPropertyName("referenzen")]
+    [ProtoContract]
     [NonOfficial(NonOfficialCategory.MISSING)]
-    [ProtoMember(1)]
+    public class Handelsunstimmigkeitsbegruendung : COM
+    {
+        /// <summary>
+        /// Referenzen auf vorherige Nachricht
+        /// </summary>
+        [JsonProperty(PropertyName = "referenzen", Required = Required.Default, Order = 1)]
+        [JsonPropertyName("referenzen")]
+        [NonOfficial(NonOfficialCategory.MISSING)]
+        [ProtoMember(1)]
 
-    public List<string> Referenzen { get; set; }
+        public List<string> Referenzen { get; set; }
 
 
-    /// <summary>
-    /// Angabe des Handelsunstimmigkeitsgrunds
-    /// </summary>
-    [JsonProperty(PropertyName = "grund", Required = Required.Always, Order = 2)]
-    [JsonPropertyName("grund")]
-    [NonOfficial(NonOfficialCategory.MISSING)]
-    [ProtoMember(2)]
-    public Handelsunstimmigkeitsgrund Grund { get; set; }
+        /// <summary>
+        /// Angabe des Handelsunstimmigkeitsgrunds
+        /// </summary>
+        [JsonProperty(PropertyName = "grund", Required = Required.Always, Order = 2)]
+        [JsonPropertyName("grund")]
+        [NonOfficial(NonOfficialCategory.MISSING)]
+        [ProtoMember(2)]
+        public Handelsunstimmigkeitsgrund Grund { get; set; }
 
-  }
+    }
 }
