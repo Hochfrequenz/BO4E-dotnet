35dbe3382a11800b59dfffa18841c8f46bf22fd9
Robert <robert.morlock@hochfrequenz.de>
2022-02-23 16:46:18 +0100

    add Rechenschritt COM

--- BO4E/COM/Rechenschritt.cs
+++ BO4E/COM/Rechenschritt.cs
@@ -13,28 +13,59 @@ namespace BO4E.COM
     public class Rechenschritt : COM
     {
         /// <summary>
-        /// Die ID des Rechenschritt (1-99999)
+        /// Die BestandteilID des Rechenschritt (1-99999)
         /// </summary>
         /// <remarks>UTILTS SG8 RFF 1154 / SG</remarks>
-        [JsonProperty(Required = Required.Always, PropertyName = "rechenschrittId")]
-        public int RechenschrittId { get; set; }
+        [JsonProperty(Required = Required.Always, PropertyName = "rechenschrittBestandteilId")]
+        [JsonPropertyName("rechenschrittBestandteilId")]
+        public int RechenschrittBestandteilId { get; set; }
+        
+        /// <summary>
+        /// Die ReferenzID des Rechenschritt (1-99999)
+        /// </summary>
+        /// <remarks>UTILTS SG8 RFF 1154 / SG</remarks>
+        [JsonProperty(Required = Required.Always, PropertyName = "referenzRechensschrittID")]
+        [JsonPropertyName("referenzRechensschrittID")]
+        public int ReferenzRechenschrittId { get; set; }
 
         /// <summary>
         /// Die Rechenoperation
         /// </summary>
-        [JsonProperty(Required = Required.Always, Order = 4, PropertyName = "operation")]
+        [JsonProperty(Required = Required.Always, Order = 3, PropertyName = "operation")]
         [JsonPropertyName("operation")]
         [BoKey]
-        [ProtoMember(4)]
+        [ProtoMember(3)]
         public ArithmetischeOperation Operation { get; set; }
-
-        // todo: add verlustfaktor
-        // todo: add leistung
-        // todo: add verweis auf messlokations id
-
+        
         /// <summary>
-        /// rekursive verschachtelung weiterer rechenschritte
+        /// Ein möglicher Trafoverlust
         /// </summary>
+        [JsonProperty(Required = Required.AllowNull, Order = 4, PropertyName = "verlustTrafo")]
+        [JsonPropertyName("verlustTrafo")]
+        [ProtoMember(4)]
+        public decimal? VerlustTrafo { get; set; }
+        
+        /// <summary>
+        /// Ein möglicher Leitungsverlust
+        /// </summary>
+        [JsonProperty(Required = Required.AllowNull, Order = 5, PropertyName = "verlustLeitung")]
+        [JsonPropertyName("verlustLeitung")]
+        [ProtoMember(5)]
+        public decimal? VerlustLeitung { get; set; }
+        
+        /// <summary>
+        /// Verweis auf MesslokationsId
+        /// </summary>
+        [JsonProperty(Required = Required.Default, Order = 6, PropertyName = "referenzMesslokationId")]
+        [JsonPropertyName("referenzMesslokationId")]
+        [ProtoMember(6)]
+        public string ReferenzMesslokationId { get; set; }
+        
+        /// <summary>
+        /// rekursive Verschachtelung weiterer rechenschritte
+        /// </summary>
+        [JsonProperty(Required = Required.Default, Order = 6, PropertyName = "weitererRechenschritt")]
+        [JsonPropertyName("weitererRechenschritt")]
         public Rechenschritt WeitererRechenschritt { get; set; }
     }
 }
