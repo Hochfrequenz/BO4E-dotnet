d3fb47f74bb09c180ebfc7e3b4a38abd672e0040
Franziska Vesely <franziska.vesely@hochfrequenz.de>
2022-02-23 19:55:26 +0100

    Add Anfrage BO to BusinessObjects and BoTyp

--- BO4E/ENUM/BoTyp.cs
+++ BO4E/ENUM/BoTyp.cs
@@ -1,66 +1,69 @@
 namespace BO4E.ENUM
 {
-    /// <summary>Auflistung sämtlicher existierender Geschäftsobjekte.</summary>
-    public enum BoTyp
-    {
-        /// <summary>1</summary>
-        ANSPRECHPARTNER,
+  /// <summary>Auflistung sämtlicher existierender Geschäftsobjekte.</summary>
+  public enum BoTyp
+  {
+    /// <summary>1</summary>
+    ANSPRECHPARTNER,
 
-        /// <summary>1</summary>
-        AVIS,
+    /// <summary>1</summary>
+    AVIS,
 
-        /// <summary>1</summary>
-        ENERGIEMENGE,
+    /// <summary>1</summary>
+    ENERGIEMENGE,
 
-        /// <summary>1</summary>
-        GESCHAEFTSOBJEKT,
+    /// <summary>1</summary>
+    GESCHAEFTSOBJEKT,
 
-        /// <summary>1</summary>
-        GESCHAEFTSPARTNER,
+    /// <summary>1</summary>
+    GESCHAEFTSPARTNER,
 
-        /// <summary>1</summary>
-        MARKTLOKATION,
+    /// <summary>1</summary>
+    MARKTLOKATION,
 
-        /// <summary>1</summary>
-        MARKTTEILNEHMER,
+    /// <summary>1</summary>
+    MARKTTEILNEHMER,
 
-        /// <summary>1</summary>
-        MESSLOKATION,
+    /// <summary>1</summary>
+    MESSLOKATION,
 
-        /// <summary>1</summary>
-        ZAEHLER,
+    /// <summary>1</summary>
+    ZAEHLER,
 
-        /// <summary>1</summary>
-        KOSTEN,
+    /// <summary>1</summary>
+    KOSTEN,
 
-        /// <summary>1</summary>
-        TARIF,
+    /// <summary>1</summary>
+    TARIF,
 
-        /// <summary>1</summary>
-        PREISBLATT,
+    /// <summary>1</summary>
+    PREISBLATT,
 
-        /// <summary>1</summary>
-        PREISBLATTNETZNUTZUNG,
+    /// <summary>1</summary>
+    PREISBLATTNETZNUTZUNG,
 
-        /// <summary>1</summary>
-        PREISBLATTMESSUNG,
+    /// <summary>1</summary>
+    PREISBLATTMESSUNG,
 
-        /// <summary>1</summary>
-        PREISBLATTUMLAGEN,
+    /// <summary>1</summary>
+    PREISBLATTUMLAGEN,
 
-        /// <summary>1</summary>
-        PREISBLATTDIENSTLEISTUNG,
+    /// <summary>1</summary>
+    PREISBLATTDIENSTLEISTUNG,
 
-        /// <summary>1</summary>
-        PREISBLATTKONZESSIONSABGABE,
+    /// <summary>1</summary>
+    PREISBLATTKONZESSIONSABGABE,
 
-        /// <summary>1</summary>
-        ZEITREIHE,
+    /// <summary>1</summary>
+    ZEITREIHE,
 
-        /// <summary>1</summary>
-        LASTGANG,
+    /// <summary>1</summary>
+    LASTGANG,
 
-        /// <summary>1</summary>
-        HANDELSUNSTIMMIGKEIT
-    }
+    /// <summary>1</summary>
+    HANDELSUNSTIMMIGKEIT,
+
+    /// <summary>1</summary>
+    ANFRAGE
+  }
 }
