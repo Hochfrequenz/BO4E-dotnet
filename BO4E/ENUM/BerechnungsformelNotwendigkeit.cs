613bda3d00397bda2cfe42849ea5058ad32fb957
rmorlock <54396093+rmorlock@users.noreply.github.com>
2022-02-24 10:39:05 +0100

    Berechnungsformel BO (#137)
    * Add Ablehngründe for Berechnungsformel
    
    * wip
    
    * wip again
    
    * wip again
    
    * wip
    
    * add Berechnungsformel BO
    
    * add BErechnungsformel to BusinessObject
    
    * add Rechenschritt COM
    
    * enhance Verwendungszweck enum with HKNR
    
    * enhance protobuf file with enums added
    
    * dotnet-format
    
    * ✒ typo ("s" zu viel)
    
    * referenzeMEsslokationsId ➡ messlokationsId
    
    * verlust*faktor*
    
    * remove data category
    
    * better comments
    
    Co-authored-by: konstantin <konstantin.klein@hochfrequenz.de>

---
+++ BO4E/ENUM/BerechnungsformelNotwendigkeit.cs
@@ -0,0 +1,29 @@
+﻿namespace BO4E.ENUM
+{
+    /// <summary>
+    /// Beschreibt, ob eine Berechnungsformel notwendig ist
+    /// </summary>
+    /// <remarks>Enspricht dem UTILTS SG5+STS Datenelement 4405 "Status der Berechnungsformel"</remarks>
+    public enum BerechnungsformelNotwendigkeit
+    {
+        /// <summary>
+        /// Berechnungsformel angefügt
+        /// </summary>
+        /// <remarks>Z33</remarks>
+        BERECHNUNGSFORMEL_NOTWENDIG,
+        /// <summary>
+        /// Berechnungsformel muss beim Absender angefragt werden
+        /// </summary>
+        /// <remarks>Z34</remarks>
+        BERECHNUNGSFORMEL_MUSS_ANGEFRAGT_WERDEN,
+        /// <summary>
+        /// Berechnungsformel besitzt keine Rechenoperation
+        /// </summary>
+        /// <remarks>Z40</remarks>
+        BERECHNUNGSFORMEL_TRIVIAL,
+        /// <summary>
+        /// Berechnungsformel ist nicht erforderlich
+        /// </summary>
+        BERECHNUNGSFORMEL_NICHT_NOTWENDIG,
+    }
+}
