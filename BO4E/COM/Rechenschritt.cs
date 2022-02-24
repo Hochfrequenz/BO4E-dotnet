201d7acae473d4791fca68e9fb79204ab6ae4f5d
konstantin <konstantin.klein@hochfrequenz.de>
2022-02-24 07:22:03 +0100

    referenzeMEsslokationsId ➡ messlokationsId

--- BO4E/COM/Rechenschritt.cs
+++ BO4E/COM/Rechenschritt.cs
@@ -56,10 +56,10 @@ namespace BO4E.COM
         /// <summary>
         /// Verweis auf MesslokationsId
         /// </summary>
-        [JsonProperty(Required = Required.Default, Order = 6, PropertyName = "referenzMesslokationId")]
-        [JsonPropertyName("referenzMesslokationId")]
+        [JsonProperty(Required = Required.Default, Order = 6, PropertyName = "messlokationId")]
+        [JsonPropertyName("messlokationId")]
         [ProtoMember(6)]
-        public string ReferenzMesslokationId { get; set; }
+        public string MesslokationId { get; set; }
 
         /// <summary>
         /// rekursive Verschachtelung weiterer rechenschritte
