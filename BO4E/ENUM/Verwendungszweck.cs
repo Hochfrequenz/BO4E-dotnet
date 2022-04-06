86d3f12004b27b4c431362dd91dac001f2a989fe
Franziska Vesely <franziska.vesely@hochfrequenz.de>
2022-04-06 10:34:26 +0200

    Add ZB5 Ausgeglichenheit Bilanzkreis zur Verwendungszweck enum

--- BO4E/ENUM/Verwendungszweck.cs
+++ BO4E/ENUM/Verwendungszweck.cs
@@ -1,24 +1,30 @@
 namespace BO4E.ENUM
 {
-    /// <summary>Verwendungungszweck der Werte Marktlokation</summary>
-    public enum Verwendungszweck
-    {
-        /// <summary>Z84: Netznutzungsabrechnung</summary>
-        NETZNUTZUNGSABRECHNUNG,
+  /// <summary>Verwendungungszweck der Werte Marktlokation</summary>
+  public enum Verwendungszweck
+  {
+    /// <summary>Z84: Netznutzungsabrechnung</summary>
+    NETZNUTZUNGSABRECHNUNG,
 
-        /// <summary>Z85: Bilanzkreisabrechnung</summary>
-        BILANZKREISABRECHNUNG,
+    /// <summary>Z85: Bilanzkreisabrechnung</summary>
+    BILANZKREISABRECHNUNG,
 
-        /// <summary>Z86: Mehrmindermbengenabrechnung</summary>
-        MEHRMINDERMBENGENABRECHNUNG,
+    /// <summary>Z86: Mehrmindermbengenabrechnung</summary>
+    MEHRMINDERMBENGENABRECHNUNG,
 
-        /// <summary>Z47: Endkundenabrechnung</summary>
-        ENDKUNDENABRECHNUNG,
+    /// <summary>Z47: Endkundenabrechnung</summary>
+    ENDKUNDENABRECHNUNG,
 
-        /// <summary>
-        /// Übermittlung an das Herkunftsnachweisregister (HKNR)
-        /// </summary>
-        /// <remarks>Z92</remarks>
-        UEBERMITTLUNG_AN_DAS_HKNR,
-    }
+    /// <summary>
+    /// Übermittlung an das Herkunftsnachweisregister (HKNR)
+    /// </summary>
+    /// <remarks>Z92</remarks>
+    UEBERMITTLUNG_AN_DAS_HKNR,
+
+    /// <summary>
+    /// Zur Ermittlung der Ausgeglichenheit von Bilanzkreisen
+    ///</summary>
+    /// <remarks>ZB5</remarks>
+    ERMITTLUNG_AUSGEGLICHENHEIT_BILANZKREIS,
+  }
 }
