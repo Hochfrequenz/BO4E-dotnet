8f6d45d6e7e28d68f28c321df1819f482deb76a8
konstantin <konstantin.klein@hochfrequenz.de>
2022-05-02 13:43:02 +0200

    make all properties in COM.Preis nullable

--- BO4E/COM/Preis.cs
+++ BO4E/COM/Preis.cs
@@ -15,21 +15,21 @@ namespace BO4E.COM
         [JsonPropertyName("wert")]
         [FieldName("value", Language.EN)]
         [ProtoMember(3)]
-        public decimal Wert { get; set; }
+        public decimal? Wert { get; set; }
 
         /// <summary>Währungseinheit für den Preis, z.B. Euro oder Ct. Details <see cref="Waehrungseinheit" /></summary>
         [JsonProperty(PropertyName = "einheit", Required = Required.Always)]
         [JsonPropertyName("einheit")]
         [FieldName("currency", Language.EN)]
         [ProtoMember(4)]
-        public Waehrungseinheit Einheit { get; set; }
+        public Waehrungseinheit? Einheit { get; set; }
 
         /// <summary>Angabe, für welche Bezugsgröße der Preis gilt. Z.B. kWh. <seealso cref="Mengeneinheit" /></summary>
         [JsonProperty(PropertyName = "bezugswert", Required = Required.Always)]
         [JsonPropertyName("bezugswert")]
         [FieldName("reference", Language.EN)]
         [ProtoMember(5)]
-        public Mengeneinheit Bezugswert { get; set; }
+        public Mengeneinheit? Bezugswert { get; set; }
 
         /// <summary>
         ///     Gibt den Status des veröffentlichten Preises an
