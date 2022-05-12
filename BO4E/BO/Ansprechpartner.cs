5bddd0c55de772fe37d32d8e6e69759c6073ad9f
konstantin <konstantin.klein@hochfrequenz.de>
2022-05-12 13:26:28 +0200

    fix conflicts

--- BO4E/BO/Ansprechpartner.cs
+++ BO4E/BO/Ansprechpartner.cs
@@ -17,9 +17,9 @@ namespace BO4E.BO
         /// <summary>
         ///     Mögliche Anrede des Ansprechpartners
         /// </summary>
-        [JsonProperty(Required = Required.Default, Order = 4, PropertyName = "anrede")]
+        [JsonProperty(Required = Required.Default, Order = 10, PropertyName = "anrede")]
         [JsonPropertyName("anrede")]
-        [JsonPropertyOrder(4)]
+        [JsonPropertyOrder(10)]
         [ProtoMember(4)]
         [DataCategory(DataCategory.NAME)]
         public Anrede? Anrede { get; set; }
@@ -29,59 +29,59 @@ namespace BO4E.BO
         ///     Anrede vorgegeben werden. Beispiel: "Sehr geehrte Frau Müller, sehr geehrter
         ///     Herr Dr. Müller"
         /// </summary>
-        [JsonProperty(Required = Required.Default, Order = 5, PropertyName = "inviduelleAnrede")]
+        [JsonProperty(Required = Required.Default, Order = 11, PropertyName = "inviduelleAnrede")]
         [JsonPropertyName("inviduelleAnrede")]
-        [JsonPropertyOrder(5)]
+        [JsonPropertyOrder(11)]
         [ProtoMember(5)]
         [DataCategory(DataCategory.NAME)]
         public string? IndividuelleAnrede { get; set; }
 
         /// <summary>Möglicher Titel des Ansprechpartners</summary>
-        [JsonProperty(Required = Required.Default, Order = 6, PropertyName = "titel")]
+        [JsonProperty(Required = Required.Default, Order = 12, PropertyName = "titel")]
         [JsonPropertyName("titel")]
-        [JsonPropertyOrder(6)]
+        [JsonPropertyOrder(12)]
         [ProtoMember(6)]
         [DataCategory(DataCategory.NAME)]
         public Titel? Titel { get; set; }
 
         /// <summary>Vorname des Ansprechpartners</summary>
-        [JsonProperty(Required = Required.Default, Order = 7, PropertyName = "vorname")]
+        [JsonProperty(Required = Required.Default, Order = 13, PropertyName = "vorname")]
         [JsonPropertyName("vorname")]
-        [JsonPropertyOrder(7)]
+        [JsonPropertyOrder(13)]
         [ProtoMember(7)]
         [DataCategory(DataCategory.NAME)]
         [BoKey]
         public string? Vorname { get; set; }
 
         /// <summary>Nachname (Familienname) des Ansprechpartners</summary>
-        [JsonProperty(Required = Required.Default, Order = 8, PropertyName = "nachname")]
+        [JsonProperty(Required = Required.Default, Order = 14, PropertyName = "nachname")]
         [JsonPropertyName("nachname")]
-        [JsonPropertyOrder(8)]
+        [JsonPropertyOrder(14)]
         [ProtoMember(8)]
         [DataCategory(DataCategory.NAME)]
         [BoKey]
         public string? Nachname { get; set; }
 
         /// <summary>E-Mail Adresse</summary>
-        [JsonProperty(Required = Required.Default, Order = 9, PropertyName = "eMailAdresse")]
+        [JsonProperty(Required = Required.Default, Order = 15, PropertyName = "eMailAdresse")]
         [JsonPropertyName("eMailAdresse")]
-        [JsonPropertyOrder(9)]
+        [JsonPropertyOrder(15)]
         [ProtoMember(9)]
         [DataCategory(DataCategory.ADDRESS)]
         public string? EMailAdresse { get; set; }
 
         /// <summary>Weitere Informationen zum Ansprechpartner</summary>
-        [JsonProperty(Required = Required.Default, Order = 10, PropertyName = "kommentar")]
+        [JsonProperty(Required = Required.Default, Order = 16, PropertyName = "kommentar")]
         [JsonPropertyName("kommentar")]
-        [JsonPropertyOrder(10)]
+        [JsonPropertyOrder(16)]
         [ProtoMember(10)]
         [DataCategory(DataCategory.NAME)]
         public string? Kommentar { get; set; }
 
         /// <summary>Der Geschäftspartner, für den dieser Ansprechpartner modelliert wird.</summary>
-        [JsonProperty(Required = Required.Default, Order = 11, PropertyName = "geschaeftspartner")]
+        [JsonProperty(Required = Required.Default, Order = 17, PropertyName = "geschaeftspartner")]
         [JsonPropertyName("geschaeftspartner")]
-        [JsonPropertyOrder(11)]
+        [JsonPropertyOrder(17)]
         [NonOfficial(NonOfficialCategory
             .UNSPECIFIED)] // it's always required in BO4E, changed it to default 2020-08-31 KK
         [ProtoMember(11)]
@@ -89,9 +89,9 @@ namespace BO4E.BO
         public Geschaeftspartner? Geschaeftspartner { get; set; }
 
         /// <summary> Adresse des Ansprechpartners, falls diese von der Adresse des Geschäftspartners abweicht.</summary>
-        [JsonProperty(Required = Required.Default, Order = 12, PropertyName = "adresse")]
+        [JsonProperty(Required = Required.Default, Order = 18, PropertyName = "adresse")]
         [JsonPropertyName("adresse")]
-        [JsonPropertyOrder(12)]
+        [JsonPropertyOrder(18)]
         [ProtoMember(12)]
         [DataCategory(DataCategory.ADDRESS)]
         public Adresse? Adresse { get; set; }
@@ -99,17 +99,17 @@ namespace BO4E.BO
         /// <summary>Liste der Telefonnummern, unter denen der Ansprechpartner erreichbar ist.</summary>
         [NonOfficial(NonOfficialCategory
             .UNSPECIFIED)] //  We suggest to name it "rufnummern" instead of "rufnummer" because it's a list")]
-        [JsonProperty(Required = Required.Default, Order = 13, PropertyName = "rufnummern")]
+        [JsonProperty(Required = Required.Default, Order = 19, PropertyName = "rufnummern")]
         [JsonPropertyName("rufnummern")]
-        [JsonPropertyOrder(13)]
+        [JsonPropertyOrder(19)]
         [ProtoMember(13)]
         [DataCategory(DataCategory.ADDRESS)]
         public List<Rufnummer>? Rufnummern { get; set; }
 
         /// <summary>Liste der Abteilungen und Zuständigkeiten des Ansprechpartners.</summary>
-        [JsonProperty(Required = Required.Default, Order = 14, PropertyName = "zustaendigkeit")]
+        [JsonProperty(Required = Required.Default, Order = 20, PropertyName = "zustaendigkeit")]
         [JsonPropertyName("zustaendigkeit")]
-        [JsonPropertyOrder(14)]
+        [JsonPropertyOrder(20)]
         [ProtoMember(14)]
         public List<Zustaendigkeit>? Zustaendigkeit { get; set; }
     }
