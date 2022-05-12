5dc20f1e3a51355e68498c64e718b3016a47ea56
konstantin <konstantin.klein@hochfrequenz.de>
2022-05-12 13:43:38 +0200

    Stable Sorting of JsonProperties in BO Bilanzierung (#207)

--- BO4E/BO/Bilanzierung.cs
+++ BO4E/BO/Bilanzierung.cs
@@ -23,8 +23,9 @@ namespace BO4E.BO
         /// <summary>
         /// Für welche Marktlokation gelten diese Bilanzierungsdaten
         /// </summary>
-        [JsonProperty(PropertyName = "marktlokationsId", Required = Required.Default)]
+        [JsonProperty(PropertyName = "marktlokationsId", Required = Required.Default, Order = 10)]
         [JsonPropertyName("marktlokationsId")]
+        [JsonPropertyOrder(10)]
         [NonOfficial(NonOfficialCategory.MISSING)]
         [ProtoMember(1000)]
         [BoKey]
@@ -32,8 +33,9 @@ namespace BO4E.BO
         /// <summary>
         /// Eine Liste der verwendeten Lastprofile (SLP, SLP/TLP, ALP etc.)
         /// </summary>
-        [JsonProperty(PropertyName = "lastprofile", Required = Required.Default)]
+        [JsonProperty(PropertyName = "lastprofile", Required = Required.Default, Order = 11)]
         [JsonPropertyName("lastprofile")]
+        [JsonPropertyOrder(11)]
         [NonOfficial(NonOfficialCategory.MISSING)]
         [ProtoMember(1001)]
         public List<Lastprofil>? Lastprofile { get; set; }
@@ -53,8 +55,9 @@ namespace BO4E.BO
         /// <summary>
         /// Inklusiver Start der Bilanzierung
         /// </summary>
-        [JsonProperty(PropertyName = "bilanzierungsbeginn", Required = Required.Default)]
+        [JsonProperty(PropertyName = "bilanzierungsbeginn", Required = Required.Default, Order = 12)]
         [JsonPropertyName("bilanzierungsbeginn")]
+        [JsonPropertyOrder(12)]
         [NonOfficial(NonOfficialCategory.MISSING)]
         [ProtoIgnore]
         public DateTimeOffset? Bilanzierungsbeginn { get; set; }
@@ -75,8 +78,9 @@ namespace BO4E.BO
         /// <summary>
         /// Exklusives Ende der Bilanzierung
         /// </summary>
-        [JsonProperty(PropertyName = "bilanzierungsende", Required = Required.Default)]
+        [JsonProperty(PropertyName = "bilanzierungsende", Required = Required.Default, Order = 13)]
         [JsonPropertyName("bilanzierungsende")]
+        [JsonPropertyOrder(13)]
         [NonOfficial(NonOfficialCategory.MISSING)]
         [ProtoIgnore]
         public DateTimeOffset? Bilanzierungsende { get; set; }
@@ -84,8 +88,9 @@ namespace BO4E.BO
         /// <summary>
         /// Bilanzkreis, should obey <see cref="EnergyIdentificationCodeExtensions.IsValidEIC"/>
         /// </summary>
-        [JsonProperty(PropertyName = "bilanzkreis", Required = Required.Default)]
+        [JsonProperty(PropertyName = "bilanzkreis", Required = Required.Default, Order = 14)]
         [JsonPropertyName("bilanzkreis")]
+        [JsonPropertyOrder(14)]
         [NonOfficial(NonOfficialCategory.MISSING)]
         [ProtoMember(1004)]
         [BoKey]
@@ -94,8 +99,9 @@ namespace BO4E.BO
         /// <summary>
         /// Jahresverbrauchsprognose
         /// </summary>
-        [JsonProperty(PropertyName = "jahresverbrauchsprognose", Required = Required.Default)]
+        [JsonProperty(PropertyName = "jahresverbrauchsprognose", Required = Required.Default, Order = 15)]
         [JsonPropertyName("jahresverbrauchsprognose")]
+        [JsonPropertyOrder(15)]
         [NonOfficial(NonOfficialCategory.MISSING)]
         [ProtoMember(1005)]
         public Menge? Jahresverbrauchsprognose { get; set; }
@@ -103,8 +109,9 @@ namespace BO4E.BO
         /// <summary>
         /// Kundenwert
         /// </summary>
-        [JsonProperty(PropertyName = "temperaturarbeit", Required = Required.Default)]
+        [JsonProperty(PropertyName = "temperaturarbeit", Required = Required.Default, Order = 16)]
         [JsonPropertyName("temperaturarbeit")]
+        [JsonPropertyOrder(16)]
         [NonOfficial(NonOfficialCategory.MISSING)]
         [ProtoMember(1018)]
         public Menge? TemperaturArbeit { get; set; }
@@ -112,22 +119,24 @@ namespace BO4E.BO
         /// <summary>
         /// Kundenwert
         /// </summary>
-        [JsonProperty(PropertyName = "kundenwert", Required = Required.Default)]
+        [JsonProperty(PropertyName = "kundenwert", Required = Required.Default, Order = 17)]
         [JsonPropertyName("kundenwert")]
+        [JsonPropertyOrder(17)]
         [NonOfficial(NonOfficialCategory.MISSING)]
         [ProtoMember(1006)]
         public Menge? Kundenwert { get; set; }
 
         /// <summary>
         ///  Verbrauchsaufteilung in % zwischen SLP und TLP-Profil
-        ///  1. [Gemessene Energiemenge der OBIS "nicht Schwachlast"] * [Verbrauchsaufteilung in % / 100%] = [zu verlagernde Energiemenge] 
+        ///  1. [Gemessene Energiemenge der OBIS "nicht Schwachlast"] * [Verbrauchsaufteilung in % / 100%] = [zu verlagernde Energiemenge]
         ///  2. [Gemessene Energiemenge der OBIS "Schwachlast"] - [zu verlagernde Energiemenge] = [Ermittelte Energiemenge für
         ///     Schwachlast]
         ///  3. [Gemessene Energiemenge der OBIS "nicht Schwachlast"] + [zu verlagernde Energiemenge] = [Ermittelte Energiemenge für nicht
         ///     Schwachlast]
         /// </summary>
-        [JsonProperty(PropertyName = "verbrauchsaufteilung", Required = Required.Default)]
+        [JsonProperty(PropertyName = "verbrauchsaufteilung", Required = Required.Default, Order = 18)]
         [JsonPropertyName("verbrauchsaufteilung")]
+        [JsonPropertyOrder(18)]
         [NonOfficial(NonOfficialCategory.MISSING)]
         [ProtoMember(1011)]
         public double? Verbrauchsaufteilung { get; set; }
@@ -135,8 +144,9 @@ namespace BO4E.BO
         /// <summary>
         ///     Zeitreihentyp (SLS, TLS, etc.)
         /// </summary>
-        [JsonProperty(PropertyName = "zeitreihentyp", Required = Required.Default)]
+        [JsonProperty(PropertyName = "zeitreihentyp", Required = Required.Default, Order = 19)]
         [JsonPropertyName("zeitreihentyp")]
+        [JsonPropertyOrder(19)]
         [NonOfficial(NonOfficialCategory.MISSING)]
         [ProtoMember(1012)]
         public Zeitreihentyp? Zeitreihentyp { get; set; }
@@ -144,8 +154,9 @@ namespace BO4E.BO
         /// <summary>
         ///     Aggregationsverantwortung
         /// </summary>
-        [JsonProperty(PropertyName = "aggregationsverantwortung", Required = Required.Default)]
+        [JsonProperty(PropertyName = "aggregationsverantwortung", Required = Required.Default, Order = 20)]
         [JsonPropertyName("aggregationsverantwortung")]
+        [JsonPropertyOrder(20)]
         [NonOfficial(NonOfficialCategory.MISSING)]
         [ProtoMember(1013)]
         public Aggregationsverantwortung? Aggregationsverantwortung { get; set; }
@@ -153,8 +164,9 @@ namespace BO4E.BO
         /// <summary>
         ///     Prognosegrundlage
         /// </summary>
-        [JsonProperty(PropertyName = "prognosegrundlage", Required = Required.Default)]
+        [JsonProperty(PropertyName = "prognosegrundlage", Required = Required.Default, Order = 21)]
         [JsonPropertyName("prognosegrundlage")]
+        [JsonPropertyOrder(21)]
         [NonOfficial(NonOfficialCategory.MISSING)]
         [ProtoMember(1013)]
         public Prognosegrundlage? Prognosegrundlage { get; set; }
@@ -164,8 +176,9 @@ namespace BO4E.BO
         ///     Besteht der Bedarf ein tagesparameteräbhängiges Lastprofil mit gemeinsamer Messung anzugeben, so ist dies über die 2 -malige
         ///     Wiederholung des CAV Segments mit der Angabe der Codes E02 und E14 möglich.
         /// </summary>
-        [JsonProperty(PropertyName = "detailsPrognosegrundlage", Required = Required.Default)]
+        [JsonProperty(PropertyName = "detailsPrognosegrundlage", Required = Required.Default, Order = 22)]
         [JsonPropertyName("detailsPrognosegrundlage")]
+        [JsonPropertyOrder(22)]
         [NonOfficial(NonOfficialCategory.MISSING)]
         [ProtoMember(1014)]
         public List<Profiltyp>? DetailsPrognosegrundlage { get; set; }
@@ -173,8 +186,9 @@ namespace BO4E.BO
         /// <summary>
         ///     Wahlrecht der Prognosegrundlage (true = Wahlrecht beim Lieferanten vorhanden)
         /// </summary>
-        [JsonProperty(PropertyName = "wahlrechtPrognosegrundlage", Required = Required.Default)]
+        [JsonProperty(PropertyName = "wahlrechtPrognosegrundlage", Required = Required.Default, Order = 23)]
         [JsonPropertyName("wahlrechtPrognosegrundlage")]
+        [JsonPropertyOrder(23)]
         [NonOfficial(NonOfficialCategory.MISSING)]
         [ProtoMember(1015)]
         public WahlrechtPrognosegrundlage? WahlrechtPrognosegrundlage { get; set; }
@@ -182,8 +196,9 @@ namespace BO4E.BO
         /// <summary>
         ///     Fallgruppenzuordnung (für gas RLM)
         /// </summary>
-        [JsonProperty(PropertyName = "fallgruppenzuordnung", Required = Required.Default)]
+        [JsonProperty(PropertyName = "fallgruppenzuordnung", Required = Required.Default, Order = 24)]
         [JsonPropertyName("fallgruppenzuordnung")]
+        [JsonPropertyOrder(24)]
         [NonOfficial(NonOfficialCategory.MISSING)]
         [ProtoMember(1016)]
         public Fallgruppenzuordnung? Fallgruppenzuordnung { get; set; }
@@ -191,8 +206,9 @@ namespace BO4E.BO
         /// <summary>
         ///   Priorität des Bilanzkreises (für Gas)
         /// </summary>
-        [JsonProperty(PropertyName = "prioritaet", Required = Required.Default)]
+        [JsonProperty(PropertyName = "prioritaet", Required = Required.Default, Order = 25)]
         [JsonPropertyName("prioritaet")]
+        [JsonPropertyOrder(25)]
         [NonOfficial(NonOfficialCategory.MISSING)]
         [ProtoMember(1017)]
         public int? Prioritaet { get; set; }
