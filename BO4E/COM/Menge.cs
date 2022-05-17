c87bb9551bc0460a12647c3ca5dd9ced274a1bb8
Matthias-René Dachner <matthias-rene.dachner@hochfrequenz.de>
2022-05-17 21:02:43 +0200

    add Korrekturfaktor to Menge

--- BO4E/COM/Menge.cs
+++ BO4E/COM/Menge.cs
@@ -27,4 +27,11 @@ namespace BO4E.COM
         [ProtoMember(4)]
         public Mengeneinheit? Einheit { get; set; }
     }
+
+       /// <summary>Gibt ggf. einen Korrekturfaktor für die Menge an.</summary>
+        [JsonProperty(PropertyName = "korrekturfaktor", Required = Required.Default)]
+        [JsonPropertyName("korrekturfaktor")]
+        [ProtoMember(5)]
+        public decimal? Korrekturfaktor { get; set; }
+    }
 }
