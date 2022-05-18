d82eefb7efac1126f612ac8e269fd0921cf03800
konstantin <konstantin.klein@hochfrequenz.de>
2022-05-18 11:53:20 +0200

    fix json ordering

--- BO4E/COM/Rechnungsposition.cs
+++ BO4E/COM/Rechnungsposition.cs
@@ -16,9 +16,9 @@ namespace BO4E.COM
     public class Rechnungsposition : COM
     {
         /// <summary>Fortlaufende Nummer für die Rechnungsposition.</summary>
-        [JsonProperty(PropertyName = "positionsnummer", Required = Required.Always)]
+        [JsonProperty(PropertyName = "positionsnummer", Required = Required.Always, Order = 11)]
         [JsonPropertyName("positionsnummer")]
-        [JsonPropertyOrder(1)]
+        [JsonPropertyOrder(11)]
         [FieldName("invoiceItemNumber", Language.EN)]
         [ProtoMember(1)]
         public int Positionsnummer { get; set; }
@@ -34,9 +34,9 @@ namespace BO4E.COM
             set => LieferungVon = DateTime.SpecifyKind(value, DateTimeKind.Utc);
         }
         /// <summary>Start der Lieferung für die abgerechnete Leistung.</summary>
-        [JsonProperty(PropertyName = "lieferungVon", Required = Required.Always)]
+        [JsonProperty(PropertyName = "lieferungVon", Required = Required.Always, Order = 12)]
         [JsonPropertyName("lieferungVon")]
-        [JsonPropertyOrder(2)]
+        [JsonPropertyOrder(12)]
         [FieldName("deliveryStart", Language.EN)]
         [ProtoIgnore]
         [Newtonsoft.Json.JsonConverter(typeof(LenientDateTimeConverter))]
@@ -53,18 +53,18 @@ namespace BO4E.COM
             set => LieferungBis = DateTime.SpecifyKind(value, DateTimeKind.Utc);
         }
         /// <summary>Ende der Lieferung für die abgerechnete Leistung.</summary>
-        [JsonProperty(PropertyName = "lieferungBis", Required = Required.Always)]
+        [JsonProperty(PropertyName = "lieferungBis", Required = Required.Always, Order = 13)]
         [JsonPropertyName("lieferungBis")]
-        [JsonPropertyOrder(3)]
+        [JsonPropertyOrder(13)]
         [FieldName("deliveryEnd", Language.EN)]
         [ProtoIgnore]
         [Newtonsoft.Json.JsonConverter(typeof(LenientDateTimeConverter))]
         public DateTimeOffset LieferungBis { get; set; }
 
         /// <summary>Bezeichnung für die abgerechnete Position.</summary>
-        [JsonProperty(PropertyName = "positionstext", Required = Required.Always)]
+        [JsonProperty(PropertyName = "positionstext", Required = Required.Always, Order = 14)]
         [JsonPropertyName("positionstext")]
-        [JsonPropertyOrder(4)]
+        [JsonPropertyOrder(14)]
         [FieldName("invoiceItemText", Language.EN)]
         [ProtoMember(4)]
         public string Positionstext { get; set; }
@@ -73,9 +73,9 @@ namespace BO4E.COM
         ///     Falls sich der Preis auf eine Zeit bezieht, steht hier die Einheit, z.B. JAHR. Details
         ///     <see cref="Mengeneinheit" />
         /// </summary>
-        [JsonProperty(PropertyName = "zeiteinheit", Required = Required.Default)]
+        [JsonProperty(PropertyName = "zeiteinheit", Required = Required.Default, Order = 15)]
         [JsonPropertyName("zeiteinheit")]
-        [JsonPropertyOrder(5)]
+        [JsonPropertyOrder(15)]
         [FieldName("unit", Language.EN)]
         [ProtoMember(5)]
         public Mengeneinheit? Zeiteinheit { get; set; }
@@ -84,23 +84,23 @@ namespace BO4E.COM
         ///     Kennzeichnung der Rechnungsposition mit der Standard-Artikelnummer des BDEW. Details
         ///     <see cref="BDEWArtikelnummer" />
         /// </summary>
-        [JsonProperty(PropertyName = "artikelnummer", Required = Required.Default)]
+        [JsonProperty(PropertyName = "artikelnummer", Required = Required.Default, Order = 16)]
         [JsonPropertyName("artikelnummer")]
-        [JsonPropertyOrder(6)]
+        [JsonPropertyOrder(16)]
         [ProtoMember(6)]
         public BDEWArtikelnummer? Artikelnummer { get; set; }
 
         /// <summary>Marktlokation, die zu dieser Position gehört.</summary>
-        [JsonProperty(PropertyName = "lokationsId", Required = Required.Default)]
+        [JsonProperty(PropertyName = "lokationsId", Required = Required.Default, Order = 17)]
         [JsonPropertyName("lokationsId")]
-        [JsonPropertyOrder(7)]
+        [JsonPropertyOrder(17)]
         [ProtoMember(7)]
         public string? LokationsId { get; set; }
 
         /// <summary>Die abgerechnete Menge mit Einheit. Z.B. 4372 kWh. Details <see cref="Menge" /></summary>
-        [JsonProperty(PropertyName = "positionsMenge", Required = Required.Default)]
+        [JsonProperty(PropertyName = "positionsMenge", Required = Required.Default, Order = 18)]
         [JsonPropertyName("positionsMenge")]
-        [JsonPropertyOrder(8)]
+        [JsonPropertyOrder(18)]
         [FieldName("amount", Language.EN)]
         [ProtoMember(8)]
         public Menge? PositionsMenge { get; set; }
@@ -109,25 +109,25 @@ namespace BO4E.COM
         ///     Eine auf die Zeiteinheit bezogene Untermenge. Z.B. bei einem Jahrespreis, 3 Monate oder 146 Tage. Basierend
         ///     darauf wird der Preis aufgeteilt. Details <see cref="Menge" />
         /// </summary>
-        [JsonProperty(PropertyName = "zeitbezogeneMenge", Required = Required.Default)]
+        [JsonProperty(PropertyName = "zeitbezogeneMenge", Required = Required.Default, Order = 19)]
         [JsonPropertyName("zeitbezogeneMenge")]
-        [JsonPropertyOrder(9)]
+        [JsonPropertyOrder(19)]
         [FieldName("timeBasedAmount", Language.EN)]
         [ProtoMember(9)]
         public Menge? ZeitbezogeneMenge { get; set; }
 
         /// <summary>Gibt ggf. einen Korrekturfaktor für die Menge an.</summary>
-        [JsonProperty(PropertyName = "korrekturfaktor", Required = Required.Default, Order = 12)]
+        [JsonProperty(PropertyName = "korrekturfaktor", Required = Required.Default, Order = 20)]
         [JsonPropertyName("korrekturfaktor")]
-        [JsonPropertyOrder(10)]
+        [JsonPropertyOrder(20)]
         [ProtoMember(10)]
         [NonOfficial(NonOfficialCategory.CUSTOMER_REQUIREMENTS)]
         public decimal? Korrekturfaktor { get; set; }
 
         /// <summary>Der Preis für eine Einheit der energetischen Menge. Details <see cref="Preis" /></summary>
-        [JsonProperty(PropertyName = "einzelpreis", Required = Required.Always)]
+        [JsonProperty(PropertyName = "einzelpreis", Required = Required.Always, Order = 21)]
         [JsonPropertyName("einzelpreis")]
-        [JsonPropertyOrder(11)]
+        [JsonPropertyOrder(21)]
         [FieldName("unitCost", Language.EN)]
         [ProtoMember(11)]
         public Preis Einzelpreis { get; set; }
@@ -136,17 +136,17 @@ namespace BO4E.COM
         ///     Das Ergebnis der Multiplikation aus einzelpreis * positionsMenge * (Faktor aus zeitbezogeneMenge). Z.B. 12,60€
         ///     * 120 kW * 3/12 (für 3 Monate). Details <see cref="Betrag" />
         /// </summary>
-        [JsonProperty(PropertyName = "teilsummeNetto", Required = Required.Default)]
+        [JsonProperty(PropertyName = "teilsummeNetto", Required = Required.Default, Order = 22)]
         [JsonPropertyName("teilsummeNetto")]
-        [JsonPropertyOrder(12)]
+        [JsonPropertyOrder(22)]
         [FieldName("subtotalNet", Language.EN)]
         [ProtoMember(12)]
         public Betrag? TeilsummeNetto { get; set; }
 
         /// <summary>Nettobetrag für den Rabatt dieser Position. Details <see cref="Betrag" /></summary>
-        [JsonProperty(PropertyName = "teilrabattNetto", Required = Required.Default)]
+        [JsonProperty(PropertyName = "teilrabattNetto", Required = Required.Default, Order = 23)]
         [JsonPropertyName("teilrabattNetto")]
-        [JsonPropertyOrder(13)]
+        [JsonPropertyOrder(23)]
         [FieldName("someDiscountNet", Language.EN)]
         [ProtoMember(13)]
         public Betrag? TeilrabattNetto { get; set; }
@@ -154,9 +154,9 @@ namespace BO4E.COM
         /// <summary>
         ///     Auf die Position entfallende Steuer, bestehend aus Steuersatz und Betrag. Details <see cref="Steuerbetrag" />
         /// </summary>
-        [JsonProperty(PropertyName = "teilsummeSteuer", Required = Required.Default)]
+        [JsonProperty(PropertyName = "teilsummeSteuer", Required = Required.Default, Order = 24)]
         [JsonPropertyName("teilsummeSteuer")]
-        [JsonPropertyOrder(14)]
+        [JsonPropertyOrder(24)]
         [FieldName("subtotalTax", Language.EN)]
         [ProtoMember(14)]
         public Steuerbetrag? TeilsummeSteuer { get; set; }
@@ -166,9 +166,9 @@ namespace BO4E.COM
         ///     Invoicing zu verarbeiten.
         ///     (Ergänzung von Hochfrequenz Unternehmensberatung GmbH)
         /// </summary>
-        [JsonProperty(PropertyName = "vertragskontoId", Required = Required.Default)]
+        [JsonProperty(PropertyName = "vertragskontoId", Required = Required.Default, Order = 25)]
         [JsonPropertyName("vertragskontoId")]
-        [JsonPropertyOrder(15)]
+        [JsonPropertyOrder(25)]
         [Obsolete("Please use vertragsId instead of vertragskontoId", false)]
         [FieldName("contractAccountId", Language.EN)]
         [ProtoMember(15)]
@@ -179,9 +179,9 @@ namespace BO4E.COM
         ///     Invoicing zu verarbeiten.
         ///     (Ergänzung von Hochfrequenz Unternehmensberatung GmbH)
         /// </summary>
-        [JsonProperty(PropertyName = "vertragsId", Required = Required.Default)]
+        [JsonProperty(PropertyName = "vertragsId", Required = Required.Default, Order = 26)]
         [JsonPropertyName("vertragsId")]
-        [JsonPropertyOrder(16)]
+        [JsonPropertyOrder(26)]
         [ProtoMember(1017)]
         [NonOfficial(NonOfficialCategory.CUSTOMER_REQUIREMENTS)]
         public string? VertragsId { get; set; }
@@ -190,9 +190,9 @@ namespace BO4E.COM
         ///     status einer Rechnungsposition in SAP Convergent Invoicing
         ///     (Ergänzung von Hochfrequenz Unternehmensberatung GmbH)
         /// </summary>
-        [JsonProperty(PropertyName = "status", Required = Required.Default)]
+        [JsonProperty(PropertyName = "status", Required = Required.Default, Order = 27)]
         [JsonPropertyName("status")]
-        [JsonPropertyOrder(17)]
+        [JsonPropertyOrder(27)]
         [NonOfficial(NonOfficialCategory.CUSTOMER_REQUIREMENTS)]
         [ProtoMember(1018)]
         public RechnungspositionsStatus? Status { get; set; }
