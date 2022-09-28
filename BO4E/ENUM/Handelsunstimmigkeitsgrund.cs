e0eec909becfd6cbb3351ae727790637a84e18ca
Franziska <73471037+hf-fvesely@users.noreply.github.com>
2022-09-28 18:23:40 +0200

    ED4FTR-29389: COMDIS additions (#260)
    * Add more values in Handelsunstimmigkeitsgrund ENUM
    
    * A04 instead of A05

--- BO4E/ENUM/Handelsunstimmigkeitsgrund.cs
+++ BO4E/ENUM/Handelsunstimmigkeitsgrund.cs
@@ -31,5 +31,41 @@ namespace BO4E.ENUM
         /// <summary> SONSTIGES_SIEHE_BEGRUENDUNG </summary>
         /// <remarks>28</remarks>
         SONSTIGES_SIEHE_BEGRUENDUNG,
+
+        /// <summary> GUELTIGES_PREISBLATT_VERSENDET </summary>
+        /// <remarks>A01</remarks>
+        GUELTIGES_PREISBLATT_VERSENDET,
+
+        /// <summary> GUELTIGER_SPERRAUFTRAG_VORHANDEN </summary>
+        /// <remarks>A02</remarks>
+        GUELTIGER_SPERRAUFTRAG_VORHANDEN,
+
+        /// <summary> KORREKTE_ARTIKEL_ID_IN_RECHNUNG </summary>
+        /// <remarks>A03</remarks>
+        KORREKTE_ARTIKEL_ID_IN_RECHNUNG,
+
+        /// <summary> KORREKTER_PREIS_ZU_GUELTIGEM_PREISBLATT_IN_RECHNUNG </summary>
+        /// <remarks>A04</remarks>
+        KORREKTER_PREIS_ZU_GUELTIGEM_PREISBLATT_IN_RECHNUNG,
+
+        /// <summary> GUELTIGES_PREISBLATT_FRISTGERECHT_VERSENDET </summary>
+        /// <remarks>A06</remarks>
+        GUELTIGES_PREISBLATT_FRISTGERECHT_VERSENDET,
+
+        /// <summary> GUELTIGE_RECHNUNG_VORHANDEN </summary>
+        /// <remarks>A07</remarks>
+        GUELTIGE_RECHNUNG_VORHANDEN,
+
+        /// <summary> KORREKTER_PREIS_IN_RECHNUNG_ABGERECHNET </summary>
+        /// <remarks>A09</remarks>
+        KORREKTER_PREIS_IN_RECHNUNG_ABGERECHNET,
+
+        /// <summary> GUELTIGES_PREISBLATT_BLINDARBEIT_VERSENDET </summary>
+        /// <remarks>A12</remarks>
+        GUELTIGES_PREISBLATT_BLINDARBEIT_VERSENDET,
+
+        /// <summary> KORREKTE_ARTIKEL_ID_FUER_ABRECHNUNG_STORNIERTER_SPERRAUFTRAG_ANGEGEBEN </summary>
+        /// <remarks>A15</remarks>
+        KORREKTE_ARTIKEL_ID_FUER_ABRECHNUNG_STORNIERTER_SPERRAUFTRAG_ANGEGEBEN,
     }
 }
