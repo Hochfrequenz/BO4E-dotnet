c146836fe8d81226aca5b87bca627bc69d437e4d
Franziska Vesely <franziska.vesely@hochfrequenz.de>
2023-01-26 15:15:56 +0100

    dotnet-format

--- BO4E/COM/Preis.cs
+++ BO4E/COM/Preis.cs
@@ -6,37 +6,37 @@ using ProtoBuf;
 
 namespace BO4E.COM
 {
-  /// <summary>Preis</summary>
-  [ProtoContract]
-  public class Preis : COM
-  {
-    /// <summary>Gibt die nomiale Höhe des Preises an.</summary>
-    [JsonProperty(PropertyName = "wert", Required = Required.Always)]
-    [JsonPropertyName("wert")]
-    [FieldName("value", Language.EN)]
-    [ProtoMember(3)]
-    public decimal? Wert { get; set; }
+    /// <summary>Preis</summary>
+    [ProtoContract]
+    public class Preis : COM
+    {
+        /// <summary>Gibt die nomiale Höhe des Preises an.</summary>
+        [JsonProperty(PropertyName = "wert", Required = Required.Always)]
+        [JsonPropertyName("wert")]
+        [FieldName("value", Language.EN)]
+        [ProtoMember(3)]
+        public decimal? Wert { get; set; }
 
-    /// <summary>Währungseinheit für den Preis, z.B. Euro oder Ct. Details <see cref="Waehrungseinheit" /></summary>
-    [JsonProperty(PropertyName = "einheit", Required = Required.Default)]
-    [JsonPropertyName("einheit")]
-    [FieldName("currency", Language.EN)]
-    [ProtoMember(4)]
-    public Waehrungseinheit? Einheit { get; set; }
+        /// <summary>Währungseinheit für den Preis, z.B. Euro oder Ct. Details <see cref="Waehrungseinheit" /></summary>
+        [JsonProperty(PropertyName = "einheit", Required = Required.Default)]
+        [JsonPropertyName("einheit")]
+        [FieldName("currency", Language.EN)]
+        [ProtoMember(4)]
+        public Waehrungseinheit? Einheit { get; set; }
 
-    /// <summary>Angabe, für welche Bezugsgröße der Preis gilt. Z.B. kWh. <seealso cref="Mengeneinheit" /></summary>
-    [JsonProperty(PropertyName = "bezugswert", Required = Required.Default)]
-    [JsonPropertyName("bezugswert")]
-    [FieldName("reference", Language.EN)]
-    [ProtoMember(5)]
-    public Mengeneinheit? Bezugswert { get; set; }
+        /// <summary>Angabe, für welche Bezugsgröße der Preis gilt. Z.B. kWh. <seealso cref="Mengeneinheit" /></summary>
+        [JsonProperty(PropertyName = "bezugswert", Required = Required.Default)]
+        [JsonPropertyName("bezugswert")]
+        [FieldName("reference", Language.EN)]
+        [ProtoMember(5)]
+        public Mengeneinheit? Bezugswert { get; set; }
 
-    /// <summary>
-    ///     Gibt den Status des veröffentlichten Preises an
-    /// </summary>
-    [JsonProperty(PropertyName = "status", Required = Required.Default)]
-    [JsonPropertyName("status")]
-    [ProtoMember(6)]
-    public Preisstatus? Status { get; set; }
-  }
+        /// <summary>
+        ///     Gibt den Status des veröffentlichten Preises an
+        /// </summary>
+        [JsonProperty(PropertyName = "status", Required = Required.Default)]
+        [JsonPropertyName("status")]
+        [ProtoMember(6)]
+        public Preisstatus? Status { get; set; }
+    }
 }
