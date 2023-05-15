054f4762070b8a0d12c67295f708e9109a40b73d
Joscha Metze <joscha.metze@hochfrequenz.de>
2023-05-15 12:12:29 +0200

    Add Gerateart enum

---
+++ BO4E/ENUM/Geraeteart.cs
@@ -0,0 +1,40 @@
+using ProtoBuf;
+
+using System.Runtime.Serialization;
+
+namespace BO4E.ENUM
+{
+    /// <summary>Auflistung möglicher Geraetearten.</summary>
+    public enum Geraeteart
+    {
+        /// <summary>Wandler</summary>
+        [ProtoEnum(Name = nameof(Geraeteart) + "_" + nameof(WANDLER))]
+        [EnumMember(Value = "WANDLER")]
+        WANDLER,
+        /// <summary>Kommunikationseinrichtung</summary>
+        [ProtoEnum(Name = nameof(Geraeteart) + "_" + nameof(KOMMUNIKATIONSEINRICHTUNG))]
+        [EnumMember(Value = "KOMMUNIKATIONSEINRICHTUNG")]
+        KOMMUNIKATIONSEINRICHTUNG,
+        /// <summary>Technische Steuereinrichtung</summary>
+        [ProtoEnum(Name = nameof(Geraeteart) + "_" + nameof(TECHNISCHE_STEUEREINRICHTUNG))]
+        [EnumMember(Value = "TECHNISCHE_STEUEREINRICHTUNG")]
+        TECHNISCHE_STEUEREINRICHTUNG,
+        /// <summary>Mengenumwerter</summary>
+        [ProtoEnum(Name = nameof(Geraeteart) + "_" + nameof(MENGENUMWERTER))]
+        [EnumMember(Value = "MENGENUMWERTER")]
+        MENGENUMWERTER,
+        /// <summary>Smartmeter-Gateway</summary>
+        [ProtoEnum(Name = nameof(Geraeteart) + "_" + nameof(SMARTMETER_GATEWAY))]
+        [EnumMember(Value = "SMARTMETER_GATEWAY")]
+        SMARTMETER_GATEWAY,
+        /// <summary>Steuerbox</summary>
+        [ProtoEnum(Name = nameof(Geraeteart) + "_" + nameof(STEUERBOX))]
+        [EnumMember(Value = "STEUERBOX")]
+        STEUERBOX,
+        /// <summary>Zaehleinrichtung</summary>
+        [ProtoEnum(Name = nameof(Geraeteart) + "_" + nameof(ZAEHLEINRICHTUNG))]
+        [EnumMember(Value = "ZAEHLEINRICHTUNG")]
+        ZAEHLEINRICHTUNG
+
+    }
+}
