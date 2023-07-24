b06379af5260467abcb83d0d1c35456e9596f763
larisa <129063905+hf-lkosareva@users.noreply.github.com>
2023-07-24 16:27:49 +0200

    DEV-24622: Enhance enum ZaehlzeitdefinitionNotwendigkeit (#348)
    enhance enum ZaehlzeitdefinitionNotwendigkeit

---
+++ BO4E/ENUM/DefinitionenNotwendigkeit.cs
@@ -0,0 +1,37 @@
+using System.Runtime.Serialization;
+
+namespace BO4E.ENUM
+{
+    /// <summary>
+    /// Beschreibt, ob Definitionen notwendig sind
+    /// </summary>
+    /// <remarks>Entspricht dem UTILTS SG5+STS Datenelement 4405 "Status der Nutzung von Definitionen"</remarks>
+    public enum DefinitionenNotwendigkeit
+    {
+        /// <summary>
+        /// Zählzeitdefinitionen werden verwendet 
+        /// </summary>
+        /// <remarks>Z02</remarks>
+        [EnumMember(Value = "ZAEHLZEITDEFINITIONEN_WERDEN_VERWENDET")]
+        ZAEHLZEITDEFINITIONEN_WERDEN_VERWENDET,
+        /// <summary>
+        /// Zählzeitdefinitionen werden nicht verwendet
+        /// </summary>
+        /// <remarks>Z03</remarks>
+        [EnumMember(Value = "ZAEHLZEITDEFINITIONEN_WERDEN_NICHT_VERWENDET")]
+        ZAEHLZEITDEFINITIONEN_WERDEN_NICHT_VERWENDET,
+
+        /// <summary>
+        /// Definitionen werden verwendet 
+        /// </summary>
+        /// <remarks>Z45</remarks>
+        [EnumMember(Value = "DEFINITIONEN_WERDEN_VERWENDET")]
+        DEFINITIONEN_WERDEN_VERWENDET,
+        /// <summary>
+        /// Definitionen werden nicht verwendet
+        /// </summary>
+        /// <remarks>Z46</remarks>
+        [EnumMember(Value = "DEFINITIONEN_WERDEN_NICHT_VERWENDET")]
+        DEFINITIONEN_WERDEN_NICHT_VERWENDET,
+    }
+}
