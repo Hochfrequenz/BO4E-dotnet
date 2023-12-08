e6cb727bb80909f6eeacfd1eb45d3ab541475e58
Hamid Hajiparvaneh <66730658+hamidhajiparvaneh@users.noreply.github.com>
2023-12-08 09:40:05 +0100

    DEV-42260: Adding SteuerbareRessource as new BO (#386)
    * adding LeistungsbeschreibungDesSteuerkanals as Enum
    
    * adding Steuerbare Ressource as BO
    
    * enum rename to SteuerkanalsLeistungsbeschreibung
    
    * adding SteuerbareRessource to proto
    
    * Id to always
    
    * fix index
    
    * remove from test json order

---
+++ BO4E/BO/SteuerbareRessource.cs
@@ -0,0 +1,54 @@
+using System.ComponentModel;
+using System.Text.Json.Serialization;
+
+using BO4E.ENUM;
+using BO4E.meta;
+
+using Newtonsoft.Json;
+
+using ProtoBuf;
+
+namespace BO4E.BO
+{
+    /// <summary>
+    /// Steuerbare Ressource BO
+    /// </summary>
+    [ProtoContract]
+    [NonOfficial(NonOfficialCategory.MISSING)]
+    public class SteuerbareRessource : BusinessObject
+    {
+        /// <summary>
+        /// Identifikationsnummer einer SteuerbareRessource
+        /// </summary>
+        /// <remarks>Edi-beispiel: LOC+Z19+C816417ST77'</remarks>
+        [DefaultValue("|null|")]
+        [JsonProperty(Required = Required.Always, Order = 10, PropertyName = "steuerbareRessourceId")]
+        [JsonPropertyName("steuerbareRessourceId")]
+        [JsonPropertyOrder(10)]
+        [DataCategory(DataCategory.DEVICE)]
+        [BoKey]
+        [ProtoMember(10)]
+        public string SteuerbareRessourceId { get; set; }
+
+        /// <summary>
+        /// Leistungsbeschreibung des Steuerkanals
+        /// </summary>
+        /// <remarks>Edi-beispiel: CAV+ZF2:Z14'</remarks>
+        [JsonProperty(Required = Required.Default, Order = 11, PropertyName = "steuerkanalsLeistungsbeschreibung")]
+        [JsonPropertyName("steuerkanalsLeistungsbeschreibung")]
+        [JsonPropertyOrder(11)]
+        [NonOfficial(NonOfficialCategory.MISSING)]
+        [ProtoMember(11)]
+        public SteuerkanalsLeistungsbeschreibung? SteuerkanalsLeistungsbeschreibung { get; set; }
+
+        /// <summary>
+        /// Angabe des Messstellenbetreibers, der der Steuerbaren Ressource zugeordnet ist.
+        /// </summary>
+        /// <remarks>Edi-beispiel: CAV+Z91:9900000000002'</remarks>
+        [JsonProperty(Required = Required.Default, Order = 12, PropertyName = "zugeordnetMSBCodeNr")]
+        [JsonPropertyOrder(12)]
+        [JsonPropertyName("zugeordnetMSBCodeNr")]
+        [ProtoMember(12)]
+        public string? ZugeordnetMSBCodeNr { get; set; }
+    }
+}
