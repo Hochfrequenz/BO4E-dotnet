8863d352d785b837dac20f1b90090cac69dfa660
Hamid <hamid.github@hochfrequenz.de>
2023-12-15 09:57:51 +0100

    E-Mobilität Enum

---
+++ BO4E/ENUM/EMobilitaetsart.cs
@@ -0,0 +1,20 @@
+using System.Runtime.Serialization;
+
+namespace BO4E.ENUM
+{
+    /// <summary>Im Falle der E-Mobilität bei <see cref="TechnischeRessourceVerbrauchsart"/>, eine genauere Angabe über die Art der E-Mobilität zu definieren.</summary>
+    public enum EMobilitaetsart
+    {
+        /// <summary>ZE6: Wallbox</summary>
+        [EnumMember(Value = "WALLBOX")]
+        WALLBOX,
+
+        /// <summary>Z87: E-Mobilitätsladesäule</summary>
+        [EnumMember(Value = "E_MOBILITAETSLADESAEULE")]
+        E_MOBILITAETSLADESAEULE,
+
+        /// <summary>ZE7: Ladepark</summary>
+        [EnumMember(Value = "LADEPARK")]
+        LADEPARK,
+    }
+}
