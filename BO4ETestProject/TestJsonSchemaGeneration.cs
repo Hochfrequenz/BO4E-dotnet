067161537f22697b3ec6a80fbf2ec700c259a940
konstantin <konstantin.klein@hochfrequenz.de>
2024-01-02 10:00:39 +0100

    Fix Json Schema Generation; Re-generate all Schemas

--- BO4ETestProject/TestJsonSchemaGeneration.cs
+++ BO4ETestProject/TestJsonSchemaGeneration.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using BO4E.BO;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json.Schema;
 
@@ -32,20 +33,23 @@ namespace TestBO4E
         }
 
 
-
+        private const int LastDataRowOffset = 40;
+        private const int MaxSchemasPerHour = 10;
         [TestMethod]
         [DataRow(0)]
         [DataRow(10)]
         [DataRow(20)]
         [DataRow(30)]
-        [DataRow(40)] // using these different data rows you to workaround the 10schema per hour limitation
+        [DataRow(LastDataRowOffset)] // using these different data rows you to workaround the 10schema per hour limitation (MaxSchemasPerHour)
 
         public void TestJSchemaFileGenerationBo(int offset)
         {
+            var relevantBusinessObjectTypes = typeof(BusinessObject).Assembly.GetTypes()
+                .Where(t => t.IsSubclassOf(typeof(BusinessObject)));
+            relevantBusinessObjectTypes.Count().Should().BeLessThan(LastDataRowOffset + MaxSchemasPerHour); // if this fails, add another data row to this test method
             try
             {
-                foreach (var type in typeof(BusinessObject).Assembly.GetTypes()
-                    .Where(t => t.IsSubclassOf(typeof(BusinessObject))).Skip(offset).Take(10))
+                foreach (var type in relevantBusinessObjectTypes.Skip(offset).Take(MaxSchemasPerHour))
                 {
                     var schema = BusinessObject.GetJsonSchema(type);
                     Assert.IsNotNull(schema);
