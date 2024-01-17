cfa273cbd59d4fd9592b83a6e6091ba5e5e1413c
konstantin <konstantin.klein@hochfrequenz.de>
2024-01-17 06:40:27 +0000

    improve test

--- BO4ETestProject/TestVerwendungszweckeDeserialization.cs
+++ BO4ETestProject/TestVerwendungszweckeDeserialization.cs
@@ -27,22 +27,48 @@ namespace TestBO4E
         }
 
         [TestMethod]
-        public void TestVerwendunszweckDeserializationNewtonsoft()
+        [DataRow(true, true)]
+        [DataRow(true, false)]
+        [DataRow(false, true)]
+        [DataRow(false, false)]
+        public void TestVerwendunszweckDeserializationNewtonsoft(bool useJsonStringEnum, bool useVerwendungszweck)
         {
             var settings = new JsonSerializerSettings
             {
-                Converters = new List<JsonConverter> { new VerwendungszweckJsonNetConverter(), new StringEnumConverter() }
+                Converters = new List<JsonConverter> { }
             };
+            if (useJsonStringEnum)
+            {
+                settings.Converters.Add(new StringEnumConverter());
+            }
+
+            if (useVerwendungszweck)
+            {
+                settings.Converters.Add(new VerwendungszweckJsonNetConverter());
+            }
             _TestVerwendungszweckeDeserialization(_malo => JsonConvert.SerializeObject(_malo, settings), str => JsonConvert.DeserializeObject<Marktlokation>(str, settings));
         }
 
         [TestMethod]
-        public void TestVerwendunszweckDeserializationSystemText()
+        [DataRow(true, true)]
+        [DataRow(true, false)]
+        // [DataRow(false, true)] // The JSON value could not be converted to BO4E.ENUM.Sparte (not what we want to test here)
+        // [DataRow(false, false)]
+        public void TestVerwendunszweckDeserializationSystemText(bool useJsonStringEnum, bool useVerwendungszweck)
         {
             var jsonOptions = new JsonSerializerOptions
             {
-                Converters = { new JsonStringEnumConverter(), new VerwendungszweckJsonConverter() }
+                Converters = { }
             };
+            if (useJsonStringEnum)
+            {
+                jsonOptions.Converters.Add(new JsonStringEnumConverter());
+            }
+
+            if (useVerwendungszweck)
+            {
+                jsonOptions.Converters.Add(new VerwendungszweckJsonConverter());
+            }
             _TestVerwendungszweckeDeserialization(_malo => JsonSerializer.Serialize(_malo, jsonOptions), str => JsonSerializer.Deserialize<Marktlokation>(str, jsonOptions));
         }
     }
