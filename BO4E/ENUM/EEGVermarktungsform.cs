25acdc8ad0e5eec8aaecd6a3dbd86ddbdd99d3cf
Hamid <hamid.github@hochfrequenz.de>
2024-07-24 14:36:52 +0200

    removing EDIFACT-Qualifier

--- BO4E/ENUM/EEGVermarktungsform.cs
+++ BO4E/ENUM/EEGVermarktungsform.cs
@@ -11,23 +11,27 @@ namespace BO4E.ENUM
     public enum EEGVermarktungsform
     {
         /// <summary>Ausfallvergütung für den Fall, dass andere Vermarktungsmethoden nicht verfügbar sind</summary>
-        [ProtoEnum(Name = nameof(EEGVermarktungsform) + "_" + nameof(Z90_AUSFALLVERGUETUNG))]
-        [EnumMember(Value = "Z90_AUSFALLVERGUETUNG")]
-        Z90_AUSFALLVERGUETUNG,
+        /// <remarks>Z90</remarks>
+        [ProtoEnum(Name = nameof(EEGVermarktungsform) + "_" + nameof(AUSFALLVERGUETUNG))]
+        [EnumMember(Value = "AUSFALLVERGUETUNG")]
+        AUSFALLVERGUETUNG,
 
         /// <summary>Marktprämie für die geförderte Direktvermarktung</summary>
-        [ProtoEnum(Name = nameof(EEGVermarktungsform) + "_" + nameof(Z91_MARKTPRAEMIE))]
-        [EnumMember(Value = "Z91_MARKTPRAEMIE")]
-        Z91_MARKTPRAEMIE,
+        /// <remarks>Z91</remarks>
+        [ProtoEnum(Name = nameof(EEGVermarktungsform) + "_" + nameof(MARKTPRAEMIE))]
+        [EnumMember(Value = "MARKTPRAEMIE")]
+        MARKTPRAEMIE,
 
         /// <summary>Sonstige Vermarktungsformen ohne gesetzliche Vergütung</summary>
-        [ProtoEnum(Name = nameof(EEGVermarktungsform) + "_" + nameof(Z92_SONSTIGE))]
-        [EnumMember(Value = "Z92_SONSTIGE")]
-        Z92_SONSTIGE,
+        /// <remarks>Z92</remarks>
+        [ProtoEnum(Name = nameof(EEGVermarktungsform) + "_" + nameof(SONSTIGE))]
+        [EnumMember(Value = "SONSTIGE")]
+        SONSTIGE,
 
         /// <summary>Vergütung nach dem Kraft-Wärme-Kopplungsgesetz (KWKG)</summary>
-        [ProtoEnum(Name = nameof(EEGVermarktungsform) + "_" + nameof(Z94_KWKG_VERGUETUNG))]
-        [EnumMember(Value = "Z94_KWKG_VERGUETUNG")]
-        Z94_KWKG_VERGUETUNG
+        /// <remarks>Z94</remarks>
+        [ProtoEnum(Name = nameof(EEGVermarktungsform) + "_" + nameof(KWKG_VERGUETUNG))]
+        [EnumMember(Value = "KWKG_VERGUETUNG")]
+        KWKG_VERGUETUNG
     }
 }
--- BO4E/ENUM/FernsteuerbarkeitStatus.cs
+++ BO4E/ENUM/FernsteuerbarkeitStatus.cs
@@ -14,24 +14,27 @@ namespace BO4E.ENUM
         /// Marktlokation ist technisch nicht fernsteuerbar.
         /// Der NB bestätigt mit der Anmeldung einer erzeugenden Marktlokation zur Direktvermarktung, dass die Anlage nicht mit einer Fernsteuerung ausgestattet ist und nicht fernsteuerbar ist. Die Voraussetzung zur Zahlung der Managementprämie für fernsteuerbare Anlagen ist nicht gegeben.
         /// </summary>
-        [ProtoEnum(Name = nameof(FernsteuerbarkeitStatus) + "_" + nameof(Z96_NICHT_FERNSTEUERBAR))]
-        [EnumMember(Value = "Z96_NICHT_FERNSTEUERBAR")]
-        Z96_NICHT_FERNSTEUERBAR,
+        /// <remarks>Z96</remarks>
+        [ProtoEnum(Name = nameof(FernsteuerbarkeitStatus) + "_" + nameof(NICHT_FERNSTEUERBAR))]
+        [EnumMember(Value = "NICHT_FERNSTEUERBAR")]
+        NICHT_FERNSTEUERBAR,
 
         /// <summary>
         /// Marktlokation ist technisch fernsteuerbar.
         /// Der NB bestätigt mit der Anmeldung einer erzeugenden Marktlokation zur Direktvermarktung, dass die Marktlokation mit einer Fernsteuerung ausgestattet, aber dem NB keine Information darüber vorliegt, dass der LF die Marktlokation fernsteuern kann. Die Voraussetzung zur Zahlung der Managementprämie für fernsteuerbare Marktlokation ist nicht gegeben.
         /// </summary>
-        [ProtoEnum(Name = nameof(FernsteuerbarkeitStatus) + "_" + nameof(Z97_TECHNISCH_FERNSTEUERBAR))]
-        [EnumMember(Value = "Z97_TECHNISCH_FERNSTEUERBAR")]
-        Z97_TECHNISCH_FERNSTEUERBAR,
+        /// <remarks>Z97</remarks>
+        [ProtoEnum(Name = nameof(FernsteuerbarkeitStatus) + "_" + nameof(TECHNISCH_FERNSTEUERBAR))]
+        [EnumMember(Value = "TECHNISCH_FERNSTEUERBAR")]
+        TECHNISCH_FERNSTEUERBAR,
 
         /// <summary>
         /// Marktlokation ist durch den Lieferanten fernsteuerbar.
         /// Der NB bestätigt mit der Anmeldung einer Marktlokation zur Direktvermarktung, dass die Marktlokation mit einer Fernsteuerung ausgestattet ist und der LF diese auch fernsteuern kann. Die Voraussetzung zur Zahlung der Managementprämie für fernsteuerbare Marktlokationen ist gegeben.
         /// </summary>
-        [ProtoEnum(Name = nameof(FernsteuerbarkeitStatus) + "_" + nameof(Z98_LIEFERANT_FERNSTEUERBAR))]
-        [EnumMember(Value = "Z98_LIEFERANT_FERNSTEUERBAR")]
-        Z98_LIEFERANT_FERNSTEUERBAR
+        /// <remarks>Z98</remarks>
+        [ProtoEnum(Name = nameof(FernsteuerbarkeitStatus) + "_" + nameof(LIEFERANT_FERNSTEUERBAR))]
+        [EnumMember(Value = "LIEFERANT_FERNSTEUERBAR")]
+        LIEFERANT_FERNSTEUERBAR
     }
 }
