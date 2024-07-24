17dd429861ee88b63d764e68a6b54003cdb37e14
Hamid <hamid.github@hochfrequenz.de>
2024-07-24 11:56:59 +0200

    ✨ Refactor enum names for clarity and consistency

--- BO4E/ENUM/FernsteuerbarkeitStatus.cs
+++ BO4E/ENUM/FernsteuerbarkeitStatus.cs
@@ -14,7 +14,7 @@ namespace BO4E.ENUM
         /// Marktlokation ist technisch nicht fernsteuerbar.
         /// Der NB bestätigt mit der Anmeldung einer erzeugenden Marktlokation zur Direktvermarktung, dass die Anlage nicht mit einer Fernsteuerung ausgestattet ist und nicht fernsteuerbar ist. Die Voraussetzung zur Zahlung der Managementprämie für fernsteuerbare Anlagen ist nicht gegeben.
         /// </summary>
-        [ProtoEnum(Name = nameof(Z96_NICHT_FERNSTEUERBAR))]
+        [ProtoEnum(Name = nameof(FernsteuerbarkeitStatus) + "_" + nameof(Z96_NICHT_FERNSTEUERBAR))]
         [EnumMember(Value = "Z96_NICHT_FERNSTEUERBAR")]
         Z96_NICHT_FERNSTEUERBAR,
 
@@ -22,7 +22,7 @@ namespace BO4E.ENUM
         /// Marktlokation ist technisch fernsteuerbar.
         /// Der NB bestätigt mit der Anmeldung einer erzeugenden Marktlokation zur Direktvermarktung, dass die Marktlokation mit einer Fernsteuerung ausgestattet, aber dem NB keine Information darüber vorliegt, dass der LF die Marktlokation fernsteuern kann. Die Voraussetzung zur Zahlung der Managementprämie für fernsteuerbare Marktlokation ist nicht gegeben.
         /// </summary>
-        [ProtoEnum(Name = nameof(Z97_TECHNISCH_FERNSTEUERBAR))]
+        [ProtoEnum(Name = nameof(FernsteuerbarkeitStatus) + "_" + nameof(Z97_TECHNISCH_FERNSTEUERBAR))]
         [EnumMember(Value = "Z97_TECHNISCH_FERNSTEUERBAR")]
         Z97_TECHNISCH_FERNSTEUERBAR,
 
@@ -30,7 +30,7 @@ namespace BO4E.ENUM
         /// Marktlokation ist durch den Lieferanten fernsteuerbar.
         /// Der NB bestätigt mit der Anmeldung einer Marktlokation zur Direktvermarktung, dass die Marktlokation mit einer Fernsteuerung ausgestattet ist und der LF diese auch fernsteuern kann. Die Voraussetzung zur Zahlung der Managementprämie für fernsteuerbare Marktlokationen ist gegeben.
         /// </summary>
-        [ProtoEnum(Name = nameof(Z98_LIEFERANT_FERNSTEUERBAR))]
+        [ProtoEnum(Name = nameof(FernsteuerbarkeitStatus) + "_" + nameof(Z98_LIEFERANT_FERNSTEUERBAR))]
         [EnumMember(Value = "Z98_LIEFERANT_FERNSTEUERBAR")]
         Z98_LIEFERANT_FERNSTEUERBAR
     }
