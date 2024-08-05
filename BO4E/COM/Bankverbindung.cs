9e56565bd0085a84da14ff0f477cabca4643b488
Hamid <hamid.github@hochfrequenz.de>
2024-08-05 14:21:12 +0200

    fixing JsonPropertyOrder numbers

--- BO4E/COM/Bankverbindung.cs
+++ BO4E/COM/Bankverbindung.cs
@@ -12,49 +12,49 @@ public class Bankverbindung : COM
 {
     /// <summary>Der kontonummer. IBAN</summary>
     [DataCategory(DataCategory.FINANCE)]
-    [JsonProperty(PropertyName = "kontonummer", Required = Required.Default, Order = 1)]
+    [JsonProperty(PropertyName = "kontonummer", Required = Required.Default, Order = 3)]
     [JsonPropertyName("kontonummer")]
     [ProtoMember(3)]
-    [JsonPropertyOrder(1)]
+    [JsonPropertyOrder(3)]
     public string? Kontonummer { get; set; }
 
     /// <summary>Der kontoinhaber</summary>
     [DataCategory(DataCategory.FINANCE)]
-    [JsonProperty(PropertyName = "kontoinhaber", Required = Required.Default, Order = 2)]
+    [JsonProperty(PropertyName = "kontoinhaber", Required = Required.Default, Order = 4)]
     [JsonPropertyName("kontoinhaber")]
     [ProtoMember(4)]
-    [JsonPropertyOrder(2)]
+    [JsonPropertyOrder(4)]
     public string? Kontoinhaber { get; set; }
 
     /// <summary>Die Bankkennung, BIC</summary>
     [DataCategory(DataCategory.FINANCE)]
-    [JsonProperty(PropertyName = "bankkennung", Required = Required.Default, Order = 3)]
+    [JsonProperty(PropertyName = "bankkennung", Required = Required.Default, Order = 5)]
     [JsonPropertyName("bankkennung")]
     [ProtoMember(5)]
-    [JsonPropertyOrder(3)]
+    [JsonPropertyOrder(5)]
     public string? Bankkennung { get; set; }
 
     /// <summary>Das bankcode</summary>
     [DataCategory(DataCategory.FINANCE)]
-    [JsonProperty(PropertyName = "bankcode", Required = Required.Default, Order = 4)]
+    [JsonProperty(PropertyName = "bankcode", Required = Required.Default, Order = 6)]
     [JsonPropertyName("bankcode")]
     [ProtoMember(6)]
-    [JsonPropertyOrder(4)]
+    [JsonPropertyOrder(6)]
     public string? Bankcode { get; set; }
 
     /// <summary>Der Bankname</summary>
     [DataCategory(DataCategory.FINANCE)]
-    [JsonProperty(PropertyName = "bankname", Required = Required.Default, Order = 5)]
+    [JsonProperty(PropertyName = "bankname", Required = Required.Default, Order = 7)]
     [JsonPropertyName("bankname")]
     [ProtoMember(7)]
-    [JsonPropertyOrder(5)]
+    [JsonPropertyOrder(7)]
     public string? Bankname { get; set; }
 
     /// <summary>Das Land</summary>
     [DataCategory(DataCategory.FINANCE)]
-    [JsonProperty(PropertyName = "land", Required = Required.Default, Order = 6)]
+    [JsonProperty(PropertyName = "land", Required = Required.Default, Order = 8)]
     [JsonPropertyName("land")]
     [ProtoMember(8)]
-    [JsonPropertyOrder(6)]
+    [JsonPropertyOrder(8)]
     public Landescode? Land { get; set; }
 }
--- BO4E/COM/Erreichbarkeit.cs
+++ BO4E/COM/Erreichbarkeit.cs
@@ -15,39 +15,45 @@ namespace BO4E.COM
     public class Erreichbarkeit : COM
     {
         /// <summary>Erreichbarkeit am Montag (Format: HHMMHHMM).</summary>
-        [JsonProperty(PropertyName = "montagErreichbarkeit", Required = Required.Default, Order = 1)]
+        [JsonProperty(PropertyName = "montagErreichbarkeit", Required = Required.Default, Order = 3)]
         [JsonPropertyName("montagErreichbarkeit")]
         [ProtoMember(3)]
+        [JsonPropertyOrder(3)]
         public string? MontagErreichbarkeit { get; set; }
 
         /// <summary>Erreichbarkeit am Dienstag (Format: HHMMHHMM).</summary>
-        [JsonProperty(PropertyName = "dienstagErreichbarkeit", Required = Required.Default, Order = 2)]
+        [JsonProperty(PropertyName = "dienstagErreichbarkeit", Required = Required.Default, Order = 4)]
         [JsonPropertyName("dienstagErreichbarkeit")]
         [ProtoMember(4)]
+        [JsonPropertyOrder(4)]
         public string? DienstagErreichbarkeit { get; set; }
 
         /// <summary>Erreichbarkeit am Mittwoch (Format: HHMMHHMM).</summary>
-        [JsonProperty(PropertyName = "mittwochErreichbarkeit", Required = Required.Default, Order = 3)]
+        [JsonProperty(PropertyName = "mittwochErreichbarkeit", Required = Required.Default, Order = 5)]
         [JsonPropertyName("mittwochErreichbarkeit")]
         [ProtoMember(5)]
+        [JsonPropertyOrder(5)]
         public string? MittwochErreichbarkeit { get; set; }
 
         /// <summary>Erreichbarkeit am Donnerstag (Format: HHMMHHMM).</summary>
-        [JsonProperty(PropertyName = "donnerstagErreichbarkeit", Required = Required.Default, Order = 4)]
+        [JsonProperty(PropertyName = "donnerstagErreichbarkeit", Required = Required.Default, Order = 6)]
         [JsonPropertyName("donnerstagErreichbarkeit")]
         [ProtoMember(6)]
+        [JsonPropertyOrder(6)]
         public string? DonnerstagErreichbarkeit { get; set; }
 
         /// <summary>Erreichbarkeit am Freitag (Format: HHMMHHMM).</summary>
-        [JsonProperty(PropertyName = "freitagErreichbarkeit", Required = Required.Default, Order = 5)]
+        [JsonProperty(PropertyName = "freitagErreichbarkeit", Required = Required.Default, Order = 7)]
         [JsonPropertyName("freitagErreichbarkeit")]
         [ProtoMember(7)]
+        [JsonPropertyOrder(7)]
         public string? FreitagErreichbarkeit { get; set; }
 
         /// <summary>Mittagspause (Ausschluss der Erreichbarkeit, Format: HHMMHHMM).</summary>
-        [JsonProperty(PropertyName = "mittagspause", Required = Required.Default, Order = 6)]
+        [JsonProperty(PropertyName = "mittagspause", Required = Required.Default, Order = 8)]
         [JsonPropertyName("mittagspause")]
         [ProtoMember(8)]
+        [JsonPropertyOrder(8)]
         public string? Mittagspause { get; set; }
     }
 }
