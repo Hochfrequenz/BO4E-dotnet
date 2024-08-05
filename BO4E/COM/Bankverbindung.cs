26364aaf301f5f244cf979a70e4d4068b6775fe7
Hamid <hamid.github@hochfrequenz.de>
2024-08-05 13:51:16 +0200

    adding Erreichbarkeit as COM

---
+++ BO4E/COM/Bankverbindung.cs
@@ -0,0 +1,60 @@
+using System.Text.Json.Serialization;
+using BO4E.ENUM;
+using BO4E.meta;
+using Newtonsoft.Json;
+using ProtoBuf;
+
+namespace BO4E.COM;
+
+/// <summary>Enthält eine Bankverbindung</summary>
+[ProtoContract]
+public class Bankverbindung : COM
+{
+    /// <summary>Der kontonummer. IBAN</summary>
+    [DataCategory(DataCategory.FINANCE)]
+    [JsonProperty(PropertyName = "kontonummer", Required = Required.Default, Order = 1)]
+    [JsonPropertyName("kontonummer")]
+    [ProtoMember(1)]
+    [JsonPropertyOrder(1)]
+    public string? Kontonummer { get; set; }
+
+    /// <summary>Der kontoinhaber</summary>
+    [DataCategory(DataCategory.FINANCE)]
+    [JsonProperty(PropertyName = "kontoinhaber", Required = Required.Default, Order = 2)]
+    [JsonPropertyName("kontoinhaber")]
+    [ProtoMember(2)]
+    [JsonPropertyOrder(2)]
+    public string? Kontoinhaber { get; set; }
+
+    /// <summary>Die Bankkennung, BIC</summary>
+    [DataCategory(DataCategory.FINANCE)]
+    [JsonProperty(PropertyName = "bankkennung", Required = Required.Default, Order = 3)]
+    [JsonPropertyName("bankkennung")]
+    [ProtoMember(3)]
+    [JsonPropertyOrder(3)]
+    public string? Bankkennung { get; set; }
+
+    /// <summary>Das bankcode</summary>
+    [DataCategory(DataCategory.FINANCE)]
+    [JsonProperty(PropertyName = "bankcode", Required = Required.Default, Order = 4)]
+    [JsonPropertyName("bankcode")]
+    [ProtoMember(4)]
+    [JsonPropertyOrder(4)]
+    public string? Bankcode { get; set; }
+
+    /// <summary>Der Bankname</summary>
+    [DataCategory(DataCategory.FINANCE)]
+    [JsonProperty(PropertyName = "bankname", Required = Required.Default, Order = 5)]
+    [JsonPropertyName("bankname")]
+    [ProtoMember(5)]
+    [JsonPropertyOrder(5)]
+    public string? Bankname { get; set; }
+
+    /// <summary>Das Land</summary>
+    [DataCategory(DataCategory.FINANCE)]
+    [JsonProperty(PropertyName = "land", Required = Required.Default, Order = 6)]
+    [JsonPropertyName("land")]
+    [ProtoMember(6)]
+    [JsonPropertyOrder(6)]
+    public Landescode? Land { get; set; }
+}
