a656cc9c70295b44c02ae73b127bfd85dc12ef66
Lev <lev.perschin@gmail.com>
2024-11-13 14:15:28 +0100

    DEV-77895: Refactor Rechnungsposition Zuschlag/Abschlag to Enums (#580)
    * refactor to enum
    
    * Add qualifier to comments
    
    * format

---
+++ BO4E/ENUM/RechnungspositionsAbschlag.cs
@@ -0,0 +1,21 @@
+using System.Runtime.Serialization;
+
+namespace BO4E.ENUM;
+
+/// <summary>
+/// Abschlag einer Rechnungsposition
+/// </summary>
+public enum RechnungspositionsAbschlag
+{
+    /// <summary>
+    /// Gemeinderabatt nach Konzessionsabgabenverordnung [Z01]
+    /// </summary>
+    [EnumMember(Value = "GEMEINDERABATT")]
+    GEMEINDERABATT,
+
+    /// <summary>
+    /// Anpassung nach § 19, Absatz 2 Stromnetzentgeltverordnung [Z04]
+    /// </summary>
+    [EnumMember(Value = "ABSCHLAG_ANPASSUNG")]
+    ABSCHLAG_ANPASSUNG,
+}
---
+++ BO4E/ENUM/RechnungspositionsZuschlag.cs
@@ -0,0 +1,33 @@
+using System.Runtime.Serialization;
+
+namespace BO4E.ENUM;
+
+/// <summary>
+/// Zuschlag einer Rechnungsposition
+/// </summary>
+public enum RechnungspositionsZuschlag
+{
+    /// <summary>
+    /// Umspannungszuschlag [Z02]
+    /// </summary>
+    [EnumMember(Value = "UMSPANNUNGSZUSCHLAG")]
+    UMSPANNUNGSZUSCHLAG,
+
+    /// <summary>
+    /// allein genutzte Betriebsmittel nach § 19, Absatz 3 Stromnetzentgeltverordnung [Z03]
+    /// </summary>
+    [EnumMember(Value = "ALLEIN_GENUTZTE_BETRIEBSMITTEL")]
+    ALLEIN_GENUTZTE_BETRIEBSMITTEL,
+
+    /// <summary>
+    /// Anpassung nach § 19, Absatz 2 Stromnetzentgeltverordnung [Z04]
+    /// </summary>
+    [EnumMember(Value = "ZUSCHLAG_ANPASSUNG")]
+    ZUSCHLAG_ANPASSUNG,
+
+    /// <summary>
+    /// Anpassung Pauschale Netzentgeltreduzierung nach § 14a EnWG auf Höhe der NNE [Z05]
+    /// </summary>
+    [EnumMember(Value = "ANPASSUNG_PAUSCHALE_NETZENTGELTREDUZIERUNG")]
+    ANPASSUNG_PAUSCHALE_NETZENTGELTREDUZIERUNG,
+}
