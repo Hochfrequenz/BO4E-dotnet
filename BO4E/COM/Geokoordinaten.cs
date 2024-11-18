122ae12262a5d5e32ce3a993f675a59333f4e9f0
Fstei <frederik.steinkamp@hochfrequenz.de>
2024-11-18 19:24:02 +0100

    csharper

--- BO4E/COM/Geokoordinaten.cs
+++ BO4E/COM/Geokoordinaten.cs
@@ -19,31 +19,31 @@ public class Geokoordinaten : COM
     [JsonPropertyName("laengengrad")]
     [ProtoMember(4)]
     public decimal? Laengengrad { get; set; }
-    
+
     /// <summary> Gibt die Östliche Koordinate eines entsprechenden Ortes an.</summary>
     [JsonProperty(PropertyName = "oestlichelaenge")]
     [JsonPropertyName("oestlichelaenge")]
     [ProtoMember(5)]
     public decimal? OestlicheLaenge { get; set; }
-    
+
     /// <summary> Gibt die Nördliche Koordinate eines entsprechenden Ortes an.</summary>
     [JsonProperty(PropertyName = "noerdlichebreite")]
     [JsonPropertyName("noerdlichebreite")]
     [ProtoMember(6)]
     public decimal? noerdlichebreite { get; set; }
-    
+
     /// <summary> Gibt die UTM Zone des Ortes an.</summary>
     [JsonProperty(PropertyName = "zone")]
     [JsonPropertyName("zone")]
     [ProtoMember(7)]
     public string? Zone { get; set; }
-    
+
     /// <summary> Gibt den Nordwert Ortes an.</summary>
     [JsonProperty(PropertyName = "nordwert")]
     [JsonPropertyName("nordwert")]
     [ProtoMember(8)]
     public string? nordwert { get; set; }
-    
+
     /// <summary> Gibt Ostwert des Ortes an.</summary>
     [JsonProperty(PropertyName = "ostwert")]
     [JsonPropertyName("ostwert")]
