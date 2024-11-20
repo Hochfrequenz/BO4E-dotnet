2f47e9bdf05e09827b27f546c3c155b127a26aee
konstantin <konstantin.klein@hochfrequenz.de>
2024-11-20 11:51:59 +0100

    fix: when using `SET_INITIAL_DATE_IF_NULL` in STJ only set MinValue if JSON value is null and runtime type is not nullable (#600) (#602)
    fix: when using `SET_INITIAL_DATE_IF_NULL` in STJ only set MinValue if json value is not nullable
    
    i think, this was the initial intention behind the converters? and there's no older/existing test that fails, so I think it's fine that way
    
    note that this does _NOT_ change the behaviour of the newtonsoft converters!
    
    Co-authored-by: Konstantin <konstantin.klein+github@hochfrequenz.de>

---
+++ BO4ETestProject/TestNullableDateTimeOffsetConverter.cs
@@ -0,0 +1,78 @@
+using System;
+using BO4E.meta.LenientConverters;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestBO4E;
+
+[TestClass]
+public class TestNullableDateTimeOffsetConverter
+{
+    public class ClassWithNullableDateTimeOffset
+    {
+        public DateTimeOffset? Foo { get; set; }
+    }
+
+    [TestMethod]
+    public void Test_Nullable_DateTimeOffset_is_not_set_to_min_value()
+    {
+        const string jsonString = "{\"Foo\":null}";
+        var lenientParsing = LenientParsing.SET_INITIAL_DATE_IF_NULL | LenientParsing.DATE_TIME;
+        var obj = System.Text.Json.JsonSerializer.Deserialize<ClassWithNullableDateTimeOffset>(
+            jsonString,
+            lenientParsing.GetJsonSerializerOptions()
+        );
+        obj.Foo.Should().BeNull();
+    }
+
+    public class ClassWithNonNullableDateTimeOffset
+    {
+        public DateTimeOffset Foo { get; set; }
+    }
+
+    [TestMethod]
+    public void Test_NonNullable_DateTimeOffset_is_set_to_min_value()
+    {
+        const string jsonString = "{\"Foo\":null}";
+        var lenientParsing = LenientParsing.SET_INITIAL_DATE_IF_NULL | LenientParsing.DATE_TIME;
+        var obj = System.Text.Json.JsonSerializer.Deserialize<ClassWithNonNullableDateTimeOffset>(
+            jsonString,
+            lenientParsing.GetJsonSerializerOptions()
+        );
+        obj.Foo.Should().Be(DateTimeOffset.MinValue);
+    }
+
+    public class ClassWithNullableDateTime
+    {
+        public DateTime? Foo { get; set; }
+    }
+
+    [TestMethod]
+    public void Test_Nullable_DateTime_is_not_set_to_min_value()
+    {
+        const string jsonString = "{\"Foo\":null}";
+        var lenientParsing = LenientParsing.SET_INITIAL_DATE_IF_NULL | LenientParsing.DATE_TIME;
+        var obj = System.Text.Json.JsonSerializer.Deserialize<ClassWithNullableDateTime>(
+            jsonString,
+            lenientParsing.GetJsonSerializerOptions()
+        );
+        obj.Foo.Should().BeNull();
+    }
+
+    public class ClassWithNonNullableDateTime
+    {
+        public DateTime Foo { get; set; }
+    }
+
+    [TestMethod]
+    public void Test_NonNullable_DateTime_is_set_to_min_value()
+    {
+        const string jsonString = "{\"Foo\":null}";
+        var lenientParsing = LenientParsing.SET_INITIAL_DATE_IF_NULL | LenientParsing.DATE_TIME;
+        var obj = System.Text.Json.JsonSerializer.Deserialize<ClassWithNonNullableDateTime>(
+            jsonString,
+            lenientParsing.GetJsonSerializerOptions()
+        );
+        obj.Foo.Should().Be(DateTime.MinValue);
+    }
+}
