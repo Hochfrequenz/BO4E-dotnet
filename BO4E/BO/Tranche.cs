88bd83e06cb7243d2fa2b7a740ea387823b4698d
Fstei <frederik.steinkamp@hochfrequenz.de>
2024-11-27 11:25:58 +0100

    Revert "Add TranchenFeld"
    This reverts commit 833ee833f689eef69fdcecd7678aa3bb4c832dd3.

--- BO4E/BO/Tranche.cs
+++ BO4E/BO/Tranche.cs
@@ -45,30 +45,20 @@ public class Tranche : BusinessObject
     public decimal? Aufteilungsmenge { get; set; }
 
     /// <summary>
-    /// Liefert zusätzliceh Spezifizierungen zu der Prozentualer Anteil der Tranche an der erzeugenden Marktlokation
-    /// in Prozent mit 2 Nachkommastellen
-    /// </summary>
-    [JsonProperty(Order = 13, PropertyName = "aufteilungsmenge")]
-    [JsonPropertyOrder(13)]
-    [JsonPropertyName("aufteilungsmenge")]
-    [ProtoMember(7)]
-    public string? SpezifizierungAufteilungsmenge { get; set; }
-
-    /// <summary>
     ///     Die OBIS-Kennzahl für die Tranche, die festlegt, welche auf die gemessene Größe mit dem Stand gemeldet wird.
     /// </summary>
-    [JsonProperty(Order = 14, PropertyName = "obisKennzahl")]
-    [JsonPropertyOrder(14)]
+    [JsonProperty(Order = 13, PropertyName = "obisKennzahl")]
+    [JsonPropertyOrder(13)]
     [JsonPropertyName("obisKennzahl")]
-    [ProtoMember(8)]
+    [ProtoMember(7)]
     public string? ObisKennzahl { get; set; }
 
     /// <summary>
     ///     Zugeordnete Marktpartner
     /// </summary>
-    [JsonProperty(Order = 15, PropertyName = "marktrollen")]
+    [JsonProperty(Order = 14, PropertyName = "marktrollen")]
     [JsonPropertyName("marktrollen")]
-    [JsonPropertyOrder(15)]
-    [ProtoMember(9)]
+    [JsonPropertyOrder(14)]
+    [ProtoMember(8)]
     public List<MarktpartnerDetails>? Marktrollen { get; set; }
 }
