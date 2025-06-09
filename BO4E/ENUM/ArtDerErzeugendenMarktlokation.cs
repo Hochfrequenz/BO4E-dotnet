291c0f2fb21d4c99d1734ed0898e4b79a08cc497
Franziska <franziska.vesely@hochfrequenz.de>
2025-06-09 19:56:39 +0200

    Format

--- BO4E/ENUM/ArtDerErzeugendenMarktlokation.cs
+++ BO4E/ENUM/ArtDerErzeugendenMarktlokation.cs
@@ -11,14 +11,18 @@ public enum ArtDerErzeugendenMarktlokation
 {
     /// <summary>EEG-Marktlokation ohne DV-Pflicht</summary>
     /// <remarks>Z33</remarks>
-    [ProtoEnum(Name = nameof(ArtDerErzeugendenMarktlokation) + "_" + nameof(EEG_MALO_OHNE_DV_PFLICHT))]
+    [ProtoEnum(
+        Name = nameof(ArtDerErzeugendenMarktlokation) + "_" + nameof(EEG_MALO_OHNE_DV_PFLICHT)
+    )]
     [EnumMember(Value = "EEG_MALO_OHNE_DV_PFLICHT")]
     [System.Text.Json.Serialization.JsonStringEnumMemberName("EEG_MALO_OHNE_DV_PFLICHT")]
     EEG_MALO_OHNE_DV_PFLICHT,
 
     /// <summary>KWKG-Marktlokation ohne DV-Pflicht</summary>
     /// <remarks>Z34</remarks>
-    [ProtoEnum(Name = nameof(ArtDerErzeugendenMarktlokation) + "_" + nameof(KWKG_MALO_OHNE_DV_PFLICHT))]
+    [ProtoEnum(
+        Name = nameof(ArtDerErzeugendenMarktlokation) + "_" + nameof(KWKG_MALO_OHNE_DV_PFLICHT)
+    )]
     [EnumMember(Value = "KWKG_MALO_OHNE_DV_PFLICHT")]
     [System.Text.Json.Serialization.JsonStringEnumMemberName("KWKG_MALO_OHNE_DV_PFLICHT")]
     KWKG_MALO_OHNE_DV_PFLICHT,
@@ -29,16 +33,21 @@ public enum ArtDerErzeugendenMarktlokation
     [EnumMember(Value = "SONSTIGE_MALO")]
     [System.Text.Json.Serialization.JsonStringEnumMemberName("SONSTIGE_MALO")]
     SONSTIGE_MALO,
+
     /// <summary>EEG-Marktlokation mit DV-Pflicht</summary>
     /// <remarks>Z37</remarks>
-    [ProtoEnum(Name = nameof(ArtDerErzeugendenMarktlokation) + "_" + nameof(EEG_MALO_MIT_DV_PFLICHT))]
+    [ProtoEnum(
+        Name = nameof(ArtDerErzeugendenMarktlokation) + "_" + nameof(EEG_MALO_MIT_DV_PFLICHT)
+    )]
     [EnumMember(Value = "EEG_MALO_MIT_DV_PFLICHT")]
     [System.Text.Json.Serialization.JsonStringEnumMemberName("EEG_MALO_MIT_DV_PFLICHT")]
     EEG_MALO_MIT_DV_PFLICHT,
 
     /// <summary>KWKG-Marktlokation mit DV-Pflicht</summary>
     /// <remarks>Z46</remarks>
-    [ProtoEnum(Name = nameof(ArtDerErzeugendenMarktlokation) + "_" + nameof(KWKG_MALO_MIT_DV_PFLICHT))]
+    [ProtoEnum(
+        Name = nameof(ArtDerErzeugendenMarktlokation) + "_" + nameof(KWKG_MALO_MIT_DV_PFLICHT)
+    )]
     [EnumMember(Value = "KWKG_MALO_MIT_DV_PFLICHT")]
     [System.Text.Json.Serialization.JsonStringEnumMemberName("KWKG_MALO_MIT_DV_PFLICHT")]
     KWKG_MALO_MIT_DV_PFLICHT,
