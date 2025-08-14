eb13b64a3a0dce4a78548b26f5817b8ad08ed307
Jenny <janet-jenny.brieke@lynq.tech>
2025-08-14 11:04:56 +0200

    extend Rechnungstyp "Abrechnung von Technik"

--- BO4E/ENUM/Rechnungstyp.cs
+++ BO4E/ENUM/Rechnungstyp.cs
@@ -94,4 +94,12 @@ public enum Rechnungstyp
         "ABRECHNUNG_VON_KONFIGURATIONEN_UNIVERSALBESTELLPROZESS"
     )]
     ABRECHNUNG_VON_KONFIGURATIONEN_UNIVERSALBESTELLPROZESS,
+
+    /// <summary>
+    /// ABRECHNUNG_TECHNIK
+    /// </summary>
+    [EnumMember(Value = "ABRECHNUNG_TECHNIK")]
+    [System.Text.Json.Serialization.JsonStringEnumMemberName("ABRECHNUNG_TECHNIK")]
+    ABRECHNUNG_TECHNIK,
+
 }
