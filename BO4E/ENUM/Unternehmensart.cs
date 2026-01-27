3bab3455bca26d7c2ae8db781f392b715c5d4365
Konstantin <konstantin.klein+github@hochfrequenz.de>
2026-01-27 16:40:00 +0100

    add some docstrings but not all

--- BO4E/ENUM/Unternehmensart.cs
+++ BO4E/ENUM/Unternehmensart.cs
@@ -1,7 +1,11 @@
 namespace BO4E.ENUM;
 
+/// <summary>
+/// kategorisiert Unternehmen nach Ihrer Marktrolle (basierend auf dem PARTIN-Datenmodell)
+/// </summary>
 public enum Unternehmensart
 {
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
     LIEFERANT,
     NETZBETREIBER,
     MESSSTELLENBETREIBER,
@@ -9,4 +13,5 @@ public enum Unternehmensart
     BILANZKOORDINATOR,
     BILANZKREISVERANTWORTLICHER,
     ENERGIESERVICEANBIETER,
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 }
