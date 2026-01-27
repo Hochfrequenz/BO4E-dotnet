f1e5e4c65fc02e44a9ed1a6c5b392dc4a5369a8b
konstantin <konstantin.klein@hochfrequenz.de>
2026-01-27 18:41:25 +0100

    fix copy paste

--- BO4E/ENUM/Unternehmensart.cs
+++ BO4E/ENUM/Unternehmensart.cs
@@ -12,37 +12,37 @@ public enum Unternehmensart
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
     [EnumMember(Value = "LIEFERANT")]
     [System.Text.Json.Serialization.JsonStringEnumMemberName("LIEFERANT")]
-    [ProtoEnum(Name = nameof(EEGVermarktungsform) + "_" + nameof(LIEFERANT))]
+    [ProtoEnum(Name = nameof(Unternehmensart) + "_" + nameof(LIEFERANT))]
     LIEFERANT,
 
     [EnumMember(Value = "NETZBETREIBER")]
     [System.Text.Json.Serialization.JsonStringEnumMemberName("NETZBETREIBER")]
-    [ProtoEnum(Name = nameof(EEGVermarktungsform) + "_" + nameof(NETZBETREIBER))]
+    [ProtoEnum(Name = nameof(Unternehmensart) + "_" + nameof(NETZBETREIBER))]
     NETZBETREIBER,
 
     [EnumMember(Value = "MESSSTELLENBETREIBER")]
     [System.Text.Json.Serialization.JsonStringEnumMemberName("MESSSTELLENBETREIBER")]
-    [ProtoEnum(Name = nameof(EEGVermarktungsform) + "_" + nameof(NETZBETREIBER))]
+    [ProtoEnum(Name = nameof(Unternehmensart) + "_" + nameof(NETZBETREIBER))]
     MESSSTELLENBETREIBER,
 
     [EnumMember(Value = "ÜBERTRAGUNGSNETZBETREIBER")]
     [System.Text.Json.Serialization.JsonStringEnumMemberName("ÜBERTRAGUNGSNETZBETREIBER")]
-    [ProtoEnum(Name = nameof(EEGVermarktungsform) + "_" + nameof(ÜBERTRAGUNGSNETZBETREIBER))]
+    [ProtoEnum(Name = nameof(Unternehmensart) + "_" + nameof(ÜBERTRAGUNGSNETZBETREIBER))]
     ÜBERTRAGUNGSNETZBETREIBER,
 
     [EnumMember(Value = "BILANZKOORDINATOR")]
     [System.Text.Json.Serialization.JsonStringEnumMemberName("BILANZKOORDINATOR")]
-    [ProtoEnum(Name = nameof(EEGVermarktungsform) + "_" + nameof(BILANZKOORDINATOR))]
+    [ProtoEnum(Name = nameof(Unternehmensart) + "_" + nameof(BILANZKOORDINATOR))]
     BILANZKOORDINATOR,
 
     [EnumMember(Value = "BILANZKREISVERANTWORTLICHER")]
     [System.Text.Json.Serialization.JsonStringEnumMemberName("BILANZKREISVERANTWORTLICHER")]
-    [ProtoEnum(Name = nameof(EEGVermarktungsform) + "_" + nameof(BILANZKREISVERANTWORTLICHER))]
+    [ProtoEnum(Name = nameof(Unternehmensart) + "_" + nameof(BILANZKREISVERANTWORTLICHER))]
     BILANZKREISVERANTWORTLICHER,
 
     [EnumMember(Value = "ENERGIESERVICEANBIETER")]
     [System.Text.Json.Serialization.JsonStringEnumMemberName("ENERGIESERVICEANBIETER")]
-    [ProtoEnum(Name = nameof(EEGVermarktungsform) + "_" + nameof(ENERGIESERVICEANBIETER))]
+    [ProtoEnum(Name = nameof(Unternehmensart) + "_" + nameof(ENERGIESERVICEANBIETER))]
     ENERGIESERVICEANBIETER,
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 }
